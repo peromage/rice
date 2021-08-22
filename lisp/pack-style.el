797187153f1f0fc76a6d0fdf21a82fa840015110
peromage <peromage@outlook.com>
2021-08-22 01:01:18 -0400

    Add Doom modeline

--- lisp/pack-style.el
+++ lisp/pack-style.el
@@ -11,7 +11,14 @@
 (use-package all-the-icons)
 
 ;; Modeline
-(use-package doom-modeline :defer t)
+(use-package doom-modeline
+  ;; https://github.com/seagle0128/doom-modeline/issues/187#issuecomment-508973014
+  :custom-face
+  (mode-line ((t (:height 0.9))))
+  (mode-line-inactive ((t (:height 0.9))))
+  :init
+  (setq doom-modeline-height 1)
+  (doom-modeline-mode 1))
 
 (provide 'pack-style)
 ;;; pack-style.el ends here
