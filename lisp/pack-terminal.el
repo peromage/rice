c930f294b74c9122956c78205789150239e7b5ff
peromage <peromage@outlook.com>
2021-08-23 17:48:04 -0400

    Add terminal supports

---
+++ lisp/pack-terminal.el
@@ -0,0 +1,13 @@
+;;; pack-terminal.el --- Terminal supports -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package vterm
+  :if (memq system-type '(gnu gnu/linux gnu/kfreebsd darwin))
+  :init
+  (setq vterm-kill-buffer-on-exit t)
+  :config
+  (use-package multi-vterm))
+
+(provide 'pack-terminal)
+;;; pack-terminal.el ends here
