f5810d75cebcaa02244c7b0c473d1a26b0844b81
peromage <peromage@outlook.com>
2021-08-29 22:32:46 -0400

    Configure for terminal on entering

--- lisp/pack-terminal.el
+++ lisp/pack-terminal.el
@@ -2,8 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun pew-pack/term-setup ()
+  "Setup for terminal on entering."
+  (setq word-wrap nil
+        truncate-lines nil
+        truncate-partial-width-windows nil)
+  (hl-line-mode -1)
+  (display-line-numbers-mode -1)
+  (display-fill-column-indicator-mode -1))
+
+;; Eshell
+(add-hook 'eshell-mode-hook #'pew-pack/term-setup)
+
+;; Libvterm
 (use-package vterm
   :if (memq system-type '(gnu gnu/linux gnu/kfreebsd darwin))
+  :hook (vterm-mode . pew-pack/term-setup)
   :init
   (setq vterm-kill-buffer-on-exit t
         vterm-max-scrollback 99999))
