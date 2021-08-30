3e662f47c1314612def0c00ef8853a1d04f42099
peromage <peromage@outlook.com>
2021-08-30 15:47:07 -0400

    Update Clangd settings

--- lisp/pack-lsp-c.el
+++ lisp/pack-lsp-c.el
@@ -3,13 +3,26 @@
 ;;; Code:
 
 (defun pew-pack/lsp-c-setup ()
-  "Initialization for C/C++ mode."
-  (setq c-basic-offset tab-width)
-  (c-set-offset 'innamespace [0])
+  "Initialization for C mode."
+  (setq c-basic-offset tab-width
+        ;; Prevent Clangd from inserting headers itself
+        lsp-clients-clangd-args
+        '("-j=2"
+          "--background-index"
+          "--clang-tidy"
+          "--completion-style=bundled"
+          "--pch-storage=memory"
+          "--header-insertion=never"
+          "--header-insertion-decorators=0"))
   (lsp))
 
+(defun pew-pack/lsp-cpp-setup ()
+  "Initialization for C++ mode."
+  (c-set-offset 'innamespace [0])
+  (pew-pack/lsp-c-setup))
+
 (add-hook 'c-mode-hook #'pew-pack/lsp-c-setup)
-(add-hook 'c++-mode-hook #'pew-pack/lsp-c-setup)
+(add-hook 'c++-mode-hook #'pew-pack/lsp-cpp-setup)
 
 (provide 'pack-lsp-c)
 ;;; pack-lsp-c.el ends here
