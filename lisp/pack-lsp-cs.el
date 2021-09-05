c9aaafe8e3eb3f80ed8cf20bd8e183f54f6a514e
peromage <peromage@outlook.com>
2021-09-05 00:37:04 -0400

    Add LSP C# support

---
+++ lisp/pack-lsp-cs.el
@@ -0,0 +1,9 @@
+;;; pack-lsp-cs.el --- C# mode -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package csharp-mode
+  :hook (csharp-mode . lsp))
+
+(provide 'pack-lsp-cs)
+;;; pack-lsp-cs.el ends here
