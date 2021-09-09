96a3b60ec172f7d9297b5fb6adb03aa3cfd3909c
peromage <peromage@outlook.com>
2021-09-09 15:27:29 -0400

    Make LSP mode work better

--- lisp/pack-lsp.el
+++ lisp/pack-lsp.el
@@ -7,7 +7,7 @@
   :bind
   (:map lsp-mode-map
         ("C-c j" . lsp-find-definition)
-        ("C-c k" . lsp-find-declaration))
+        ("C-c J" . lsp-find-declaration))
   :init
   (setq lsp-auto-configure t
         lsp-enable-symbol-highlighting t
@@ -15,9 +15,36 @@
         lsp-headerline-breadcrumb-enable t
         lsp-idle-delay 0.5
         lsp-session-file (expand-file-name "cache/.lsp-session-v1" user-emacs-directory)
-        lsp-keymap-prefix "C-c l")
+        lsp-keymap-prefix "C-c l"
+        lsp-eldoc-enable-hover t
+        lsp-eldoc-render-all nil
+        ;;lsp-signature-auto-activate nil
+        lsp-signature-doc-lines 1
+        lsp-signature-render-documentation nil
+        lsp-modeline-diagnostics-enable t
+        lsp-modeline-code-actions-enable t
+        lsp-completion-show-detail t
+        lsp-completion-show-kind t
+        lsp-headerline-breadcrumb-enable t)
   :config
   (lsp-enable-which-key-integration 1))
+
+(defun pew-lsp/lsp-ui-doc-glance ()
+  "Quick peek documentation for the current symbol."
+  (interactive)
+  (if (lsp-ui-doc--frame-visible-p)
+      (lsp-ui-doc-focus-frame)
+    (lsp-ui-doc-glance)))
+
+(defun pew-lsp/lsp-ui-doc-toggle ()
+  "Toggle doc frame."
+  (interactive)
+  (lsp-ui-doc-mode 'toggle))
+
+(defun pew-lsp/lsp-ui-setup ()
+  "Setup function for lsp-ui."
+  (lsp-ui-mode 1)
+  (lsp-ui-doc-frame-mode 1))
 
 (use-package lsp-ui
   :after lsp-mode
@@ -26,28 +53,28 @@
   (:map lsp-ui-mode-map
         ([remap xref-find-references] . lsp-ui-peek-find-references)
         ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-        ("C-c u" . lsp-ui-imenu))
-  :hook (lsp-mode . lsp-ui-mode)
+        ("C-c u" . lsp-ui-imenu)
+        ("C-c k" . pew-lsp/lsp-ui-doc-glance)
+        ("C-c K" . pew-lsp/lsp-ui-doc-toggle))
+  :hook (lsp-mode . pew-lsp/lsp-ui-setup)
   :init
   (setq lsp-ui-peek-enable t
         lsp-ui-peek-show-directory t
         lsp-ui-sideline-enable nil
         lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-symbol nil
         lsp-ui-sideline-show-diagnostics t
         lsp-ui-sideline-show-code-actions t
         lsp-ui-sideline-update-mode 'point
         lsp-ui-sideline-delay 1
-        lsp-ui-doc-enable t
+        lsp-ui-doc-enable nil
         lsp-ui-doc-position 'bottom
         lsp-ui-doc-show-with-cursor t
         lsp-ui-doc-show-with-mouse t
-        lsp-ui-doc-delay 1
-        lsp-eldoc-enable-hover nil
-        lsp-modeline-diagnostics-enable t
-        lsp-modeline-code-actions-enable t
-        lsp-completion-show-detail t
-        lsp-completion-show-kind t
-        lsp-headerline-breadcrumb-enable t))
+        lsp-ui-doc-delay 0
+        lsp-ui-imenu-window-width 0
+        lsp-ui-imenu-auto-refresh t
+        lsp-ui-imenu-refresh-delay 1.0))
 
 (use-package lsp-ivy
   :requires ivy
