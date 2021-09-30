962ce32ec996a71e1f34cd337a35be36b0cde160
peromage <peromage@outlook.com>
2021-09-30 19:33:09 -0400

    Improve readability of vanilla settings

--- lisp/init-vanilla.el
+++ lisp/init-vanilla.el
@@ -21,11 +21,10 @@
 (add-hook 'after-init-hook #'pew/start-emacs-daemon)
 
 ;;==============================================================================
-;; Settings by variables
+;; Visual
 ;;==============================================================================
 
 (setq-default
-
  ;; Startup
  inhibit-startup-screen t
  inhibit-startup-buffer-menu t
@@ -33,36 +32,78 @@
  ;; it is really bothering See:
  ;; https://yann.hodique.info/blog/rant-obfuscation-in-emacs/
  inhibit-startup-echo-area-message user-login-name
+ ;; No annoying bell
+ ring-bell-function 'ignore
+ visible-bell nil)
 
- ;; Appearance
- display-line-numbers-type 'relative
- cursor-type 'bar
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(column-number-mode 1)
+(size-indication-mode 1)
+
+;;==============================================================================
+;; Editor
+;;==============================================================================
+
+(setq-default
+ ;; Default major mode
+ major-mode 'text-mode
+ ;; Scratch buffer message
+ initial-scratch-message ""
+ ;; Whitespaces
  whitespace-style '(face trailing tab-mark)
-
+ ;; Ruler
+ fill-column 80
+ comment-column 80
+ adaptive-fill-mode nil
+ display-fill-column-indicator-column 80
+ ;; Line number
+ display-line-numbers-type 'relative
+ ;; Cursor
+ cursor-type 'bar
  ;; No line wrap by default
  word-wrap t
  line-move-visual nil
  truncate-lines t
  truncate-partial-width-windows nil
-
  ;; Indentation
  indent-tabs-mode nil
  tab-width 4
  backward-delete-char-untabify-method 'hungry
+ ;; Final newline
+ require-final-newline t
+ mode-require-final-newline t)
 
- ;; Ruler
- fill-column 80
- comment-column 80
- display-fill-column-indicator-column 80
- adaptive-fill-mode nil
+(global-whitespace-mode 1)
+(global-display-fill-column-indicator-mode 1)
+(global-visual-line-mode -1)
+(global-display-line-numbers-mode 1)
+(show-paren-mode 1)
+(auto-fill-mode -1)
+(delete-selection-mode 1)
+(global-hl-line-mode 1)
+(blink-cursor-mode -1)
+;; Make backspace and del keys behave as expected
+(normal-erase-is-backspace-mode 1)
 
- ;; No annoying bell
- ring-bell-function 'ignore
- visible-bell nil
+;;==============================================================================
+;; Windows
+;;==============================================================================
 
- ;; Default major mode
- major-mode 'text-mode
-
+(setq-default
+ ;; Window splitting behavior
+ split-width-threshold nil
+ split-height-threshold 0
+ help-window-select t
+ ;; Splits properly
+ display-buffer-base-action '((display-buffer-reuse-window display-buffer-below-selected)
+                              (inhibit-switch-frame . t)
+                              (reusable-frames . nil))
+ ;; Better minibuffer
+ enable-recursive-minibuffers t
+ resize-mini-windows 'grow-only
+ max-mini-window-height 0.3
  ;; Better scrolling
  mouse-wheel-scroll-amount '(2 ((shift) . 5))
  mouse-wheel-progressive-speed nil
@@ -79,49 +120,15 @@
  ;; Horizontal scrolling
  hscroll-step 1
  hscroll-margin 0
- auto-hscroll-mode t
+ auto-hscroll-mode t)
 
- ;; Window splitting behavior
- split-width-threshold nil
- split-height-threshold 0
- help-window-select t
- ;; Splits properly
- display-buffer-base-action
- '((display-buffer-reuse-window display-buffer-below-selected)
-   (inhibit-switch-frame . t)
-   (reusable-frames . nil))
+(winner-mode 1)
 
- ;; Ido mode settings
- ido-enable-flex-matching t
- ido-default-file-method 'selected-window
- ido-default-buffer-method 'selected-window
+;;==============================================================================
+;; Tabbar
+;;==============================================================================
 
- ;; Input method for emergency
- default-input-method "chinese-py"
-
- ;; Local files
- ido-save-directory-list-file (expand-file-name "cache/ido.last" user-emacs-directory)
- bookmark-default-file (expand-file-name "cache/bookmarks" user-emacs-directory)
- recentf-save-file (expand-file-name "cache/recentf" user-emacs-directory)
- ;; No lock files
- create-lockfiles nil
- ;; No backup and auto-save
- make-backup-files nil
- auto-save-timeout 0
- auto-save-interval 0
-
- ;; Scratch buffer message
- initial-scratch-message ""
-
- ;; Better minibuffer
- enable-recursive-minibuffers t
- resize-mini-windows 'grow-only
- max-mini-window-height 0.3
-
- ;; Final newline
- require-final-newline t
- mode-require-final-newline t
-
+(setq-default
  ;; Tab bar
  ;; Since Emacs 27 the built-in tab is pretty viable to be used as workspace
  ;; management like Eyebrowse.  This package is aimed to make it hadier.
@@ -136,51 +143,64 @@
  tab-bar-show 1
  tab-bar-tab-hints t
  tab-bar-tab-name-function 'tab-bar-tab-name-current
- tool-bar-mode nil
-
- ;; Electric settings
- ;; No closing pair if the open pair precedes a non-whitespace character
- electric-pair-preserve-balance nil
- electric-pair-delete-adjacent-pairs nil)
+ tool-bar-mode nil)
 
 ;;==============================================================================
-;; Settings by functions
-;;==============================================================================
-
-;; Appearance
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(column-number-mode 1)
-(size-indication-mode 1)
-(show-paren-mode 1)
-(global-display-line-numbers-mode 1)
-(global-visual-line-mode -1)
-(global-whitespace-mode 1)
-(global-hl-line-mode 1)
-(blink-cursor-mode -1)
-(global-display-fill-column-indicator-mode 1)
-
-;; Convenience
-(auto-fill-mode -1)
-(delete-selection-mode 1)
-(winner-mode 1)
-(recentf-mode 1)
-(ido-mode 1)
-;; Make backspace and del keys behave as expected
-(normal-erase-is-backspace-mode 1)
-
-;; Electric helpers
-(electric-pair-mode 1)
-(electric-indent-mode 1)
-
 ;; Encoding
+;;==============================================================================
+
+(setq-default
+ ;; Input method for emergency
+ default-input-method "chinese-py")
+
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8-unix)
 (set-buffer-file-coding-system 'utf-8-unix)
 
+;;==============================================================================
+;; Misc
+;;==============================================================================
+
+(setq-default
+ ;; Local files
+ bookmark-default-file (expand-file-name "cache/bookmarks" user-emacs-directory)
+ recentf-save-file (expand-file-name "cache/recentf" user-emacs-directory)
+ ;; No lock files
+ create-lockfiles nil
+ ;; No backup and auto-save
+ make-backup-files nil
+ auto-save-timeout 0
+ auto-save-interval 0)
+
+(recentf-mode 1)
 ;; Replaces the default crap buffer manager with ibuffer
 (defalias 'list-buffers 'ibuffer)
+
+
+;;==============================================================================
+;; Electric mode
+;;==============================================================================
+
+(setq-default
+ ;; No closing pair if the open pair precedes a non-whitespace character
+ electric-pair-preserve-balance nil
+ electric-pair-delete-adjacent-pairs nil)
+
+(electric-pair-mode 1)
+(electric-indent-mode 1)
+
+;;==============================================================================
+;; Ido mode
+;;==============================================================================
+
+(setq-default
+ ;; Ido mode settings
+ ido-enable-flex-matching t
+ ido-default-file-method 'selected-window
+ ido-default-buffer-method 'selected-window
+ ido-save-directory-list-file (expand-file-name "cache/ido.last" user-emacs-directory))
+
+(ido-mode 1)
 
 ;;==============================================================================
 ;; Keybindings
