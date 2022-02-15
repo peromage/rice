7b4ae58bf14fb10ca0f0e414e1fb83390374ebe0
peromage <peromage@outlook.com>
2022-02-15 12:11:58 -0500

    Disable alternate mode for Dired

--- lisp/pack-navigation.el
+++ lisp/pack-navigation.el
@@ -10,15 +10,16 @@
   :ensure nil
   :commands (dired dired-jump dired-find-file)
   :bind (("C-x C-d" . (lambda () (interactive) (find-file default-directory)))
-         :map dired-mode-map
-         ("RET" . dired-find-alternate-file)
-         ("DEL" . (lambda () (interactive) (find-alternate-file ".."))))
+         ;;:map dired-mode-map
+         ;;("RET" . dired-find-alternate-file)
+         ;;("DEL" . (lambda () (interactive) (find-alternate-file "..")))
+         )
   :init
   (setq dired-listing-switches "-alFD --group-directories-first"
         dired-dwim-target t
         dired-recursive-copies 'always
         dired-recursive-deletes 'always)
-  (put 'dired-find-alternate-file 'disabled nil)
+  ;;(put 'dired-find-alternate-file 'disabled nil)
   :config
   (use-package dired-x :ensure nil))
 
