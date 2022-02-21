9a7472085932be2150cda31db54cdfdead5f3928
peromage <peromage@outlook.com>
2022-02-20 20:04:19 -0500

    Update lsp-pwsh

--- lisp/pkg-lsp-powershell.el
+++ lisp/pkg-lsp-powershell.el
@@ -3,7 +3,11 @@
 ;;; Code:
 
 (use-package powershell
-  :hook (powershell-mode . lsp-deferred))
+  :hook (powershell-mode . lsp-deferred)
+  ;;:custom
+  ;; Explicitely set this if Emacs cannot find it
+  ;;(lsp-pwsh-exe (expand-file-name ".dotnet/tools/pwsh" (getenv "HOME")))
+  )
 
 (provide 'pkg-lsp-powershell)
 ;;; pkg-lsp-powershell.el  ends here
