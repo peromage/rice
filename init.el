aefe0a7be35fee09ba5eb78d9cbe8c3907e287ce
peromage <peromage@outlook.com>
2022-05-04 11:49:00 -0400

    Move boot sequence to init-boot

--- init.el
+++ init.el
@@ -10,39 +10,23 @@
 ;; Initialization
 ;;------------------------------------------------------------------------------
 
-(let ((minimal-emacs-version "27"))
-  (when (version< emacs-version minimal-emacs-version)
-    (error "Emacs' version is too old.  Please use %s and above" minimal-emacs-version)))
+(defvar pew/home-dir (file-name-directory load-file-name)
+  "The PEW configuration's home directory.
+Not necessarily to be `user-emacs-directory' since this configuration can be loaded from other places.")
 
-(setq pew/home-dir (file-name-directory load-file-name)
-      ;; Variables configured via the interactive 'customize' interfaces
-      ;; Local changes that are not tracked by VCS can go in here as well
-      ;; Although some custom variables will be loaded twice the startup speed
-      ;; is not dragged down that much. I can live with that.
-      custom-file (expand-file-name "local.el" user-emacs-directory))
+;; Configurations from the interactive `customize' interfaces.
+;; Any disposable code can be put in this file.
+(setq custom-file (expand-file-name "local.el" user-emacs-directory))
 
 ;; Runtime path
 (add-to-list 'load-path (expand-file-name "lisp" pew/home-dir))
-
-;; Adjust garbage collection thresholds during startup, and thereafter
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
-
-;; Start a daemon if it is not running yet
-(add-hook 'after-init-hook
-          (lambda ()
-            (require 'server)
-            (unless (server-running-p)
-              (message "[pew] Starting Emacs daemon")
-              (server-start))))
 
 ;;------------------------------------------------------------------------------
 ;; Bootstrap -- To avoid  nested loading, all packages are managed here
 ;;------------------------------------------------------------------------------
 
 ;; Base setup
+(require 'init-boot)
 (require 'init-common)
 (require 'init-custom)
 (require 'init-keymaps)
---
+++ lisp/init-boot.el
@@ -0,0 +1,30 @@
+;;; init-boot.el --- Early initialization -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; This file should be required at the beginning of the entire initialization.
+
+;;; Code:
+
+;;;; Check minimal version
+(let ((minimal-emacs-version "27"))
+  (when (version< emacs-version minimal-emacs-version)
+    (error "Emacs' version is too old.  Please use %s and above" minimal-emacs-version)))
+
+;;;; Adjust garbage collection thresholds during startup, and thereafter
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;;;; Start a daemon if it is not running yet
+(defun pew/start--daemon()
+  "Start Emacs daemon if not running."
+  (require 'server)
+  (unless (server-running-p)
+    (message "[pew] Starting Emacs daemon")
+    (server-start)))
+(add-hook 'after-init-hook #'pew/start--daemon)
+
+(provide 'init-boot)
+;;; init-boot.el ends here
