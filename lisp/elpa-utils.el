60c05ecf8e8272904ee12c0cf57434d64474dcd1
peromage <peromage@outlook.com>
2022-05-11 15:01:18 -0400

    Merge elpa-shell and elpa-navigation into elpa-utils

--- lisp/elpa-utils.el
+++ lisp/elpa-utils.el
@@ -1,7 +1,7 @@
-;;; elpa-navigation.el --- UI navigation -*- lexical-binding: t -*-
+;;; elpa-utils.el --- Utilities -*- lexical-binding: t -*-
 ;;; Commentary:
 
-;; This module configures packages that improve navigation experience.
+;; Utilities that provide convenience and enhance experience
 
 ;;; Code:
 
@@ -31,8 +31,29 @@
   :config
   (projectile-mode 1))
 
+;;;; Search
+
 (use-package rg
   :defer t)
 
-(provide 'elpa-navigation)
-;;; elpa-navigation.el ends here
+;;;; Terminal
+
+;; Vterm is a decent terminal emulator inside of Emacs.
+;; NOTE: Not available on Windows.
+(defun pew/vterm/new ()
+  "Create a new vterm window with a unique name."
+  (interactive)
+  (vterm)
+  (set-buffer "*vterm*")
+  (rename-buffer "vterm" t))
+
+(use-package vterm
+  :if (memq system-type '(gnu gnu/linux gnu/kfreebsd darwin))
+  :hook (vterm-mode . pew/term-setup)
+  :commands (vterm vterm-other-window)
+  :custom
+  (vterm-kill-buffer-on-exit t)
+  (vterm-max-scrollback 10000))
+
+(provide 'elpa-utils)
+;;; elpa-utils.el ends here
