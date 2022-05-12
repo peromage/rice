96a2b42ed9a2570db1501ae074479e396ebe8824
peromage <peromage@outlook.com>
2022-05-12 12:29:34 -0400

    Make init-package clearer

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -5,32 +5,36 @@
 
 ;;; Code:
 
+;;;; Emacs builtin package manager
+
 ;; Initialize package manager
 (require 'package)
 
-;; Package repositories
-(dolist (archive '(("melpa" . "https://melpa.org/packages/")
-                   ;("melpa-stable" . "https://stable.melpa.org/packages/")
-                   ))
-  (add-to-list 'package-archives archive t))
-
-;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
-(setq package-user-dir (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version) user-emacs-directory)
-      package-enable-at-startup nil)
+;; Package contents
+(setq package-enable-at-startup nil
+      ;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
+      package-user-dir (locate-user-emacs-file (format "elpa-%s.%s" emacs-major-version emacs-minor-version))
+      ;; Package repositories
+      package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/"))
+      package-archive-priorities '(("melpa" . 100)
+                                   ("gnu" . 99)))
 
 ;; Fire up package.el
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Use "use-package" to simplify package configurations
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package)
-  (package-install 'diminish))
+;;;; Use `use-package' to simplify package configuration
+
+;; Install `use-package'
 (eval-when-compile
+  (unless (package-installed-p 'use-package)
+    (package-install 'use-package))
   (message "[pew] Loading use-package")
-  (require 'use-package)
-  (require 'diminish))
+  (require 'use-package))
 
 ;; Default `use-package' behaviors
 (setq use-package-always-ensure t
@@ -39,6 +43,9 @@
       use-package-always-pin nil
       use-package-compute-statistics nil
       use-package-verbose nil)
+
+;; `use-package' complementary packages
+(use-package diminish :ensure t)
 
 (provide 'init-package)
 ;;; init-package.el ends here
