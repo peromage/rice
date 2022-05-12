cbcf5a6d0071a65cd3ce546b8568ac738f761146
peromage <peromage@outlook.com>
2022-05-12 01:10:37 -0400

    Move which-key to ivy module

--- lisp/elpa-ivy.el
+++ lisp/elpa-ivy.el
@@ -81,5 +81,18 @@
   :after (ivy lsp)
   :commands lsp-ivy-workspace-symbol)
 
+;; Which-key is very informative to show keybindings when you forget them.
+(use-package which-key
+  :diminish which-key-mode
+  :custom
+  (which-key-popup-type 'side-window)
+  (which-key-show-early-on-C-h nil)
+  (which-key-idle-delay 1.0)
+  :config
+  (which-key-mode 1)
+  ;; Minibuffer usually causes display problems
+  ;(which-key-setup-minibuffer)
+  (which-key-setup-side-window-bottom))
+
 (provide 'elpa-ivy)
 ;;; elpa-ivy.el ends here
--- lisp/elpa-utils.el
+++ lisp/elpa-utils.el
@@ -5,21 +5,6 @@
 
 ;;; Code:
 
-;;;; Prompt
-
-;; Which-key is very informative to show keybindings when you forget them.
-(use-package which-key
-  :diminish which-key-mode
-  :custom
-  (which-key-popup-type 'side-window)
-  (which-key-show-early-on-C-h nil)
-  (which-key-idle-delay 1.0)
-  :config
-  (which-key-mode 1)
-  ;; Minibuffer usually causes display problems
-  ;(which-key-setup-minibuffer)
-  (which-key-setup-side-window-bottom))
-
 ;;;; Project management
 
 ;; Projectile provides a convenient way navigate between different projects.
