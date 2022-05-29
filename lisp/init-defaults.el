866d96a77f107fe086f2805deef72e3e76d337ac
peromage <peromage@outlook.com>
2022-05-29 00:58:08 -0400

    Update key bindings for other window control

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -232,11 +232,12 @@
  "r" #'winner-redo
  "u" #'winner-undo
  "q" #'pew/close-window
- "C-f" #'pew/scroll-other-window-page-down
- "C-b" #'pew/scroll-other-window-page-up
- "C-e" #'pew/scroll-other-window-line-down
- "C-y" #'pew/scroll-other-window-line-up
- "C-l" #'pew/recenter-other-window
+ ;; Other window
+ "M-f" #'pew/scroll-other-window-page-down
+ "M-b" #'pew/scroll-other-window-page-up
+ "M-e" #'pew/scroll-other-window-line-down
+ "M-y" #'pew/scroll-other-window-line-up
+ "M-l" #'pew/recenter-other-window
 
  ;; Buffers
  "n" #'pew/next-buffer
