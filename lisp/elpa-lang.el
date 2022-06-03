85638d7d2b78150182348ecf4c731839e60f992e
peromage <peromage@outlook.com>
2022-06-03 12:21:41 -0400

    Add json-mode package

--- lisp/elpa-lang.el
+++ lisp/elpa-lang.el
@@ -8,6 +8,7 @@
 ;; If the mode needs to be configured further, move it to an individual module.
 (use-package vimrc-mode :defer t)
 (use-package yaml-mode :defer t)
+(use-package json-mode :defer t)
 
 (provide 'elpa-lang)
 ;;; elpa-lang.el ends here
