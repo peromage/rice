f2b3bdbf77cbf8e05081be23d7da5957dc25e7a7
peromage <peromage@outlook.com>
2022-06-09 17:10:21 -0400

    Correct pwsh theme files

--- rice/shell/pwsh/theme-minimalist.ps1
+++ rice/shell/pwsh/theme-minimalist.ps1
@@ -1,4 +1,4 @@
-### theme-minimalist.ps1 -- Normal mediocre prompt
+### theme-minimalist.ps1 -- My pwsh prompt. It is lite
 
 function global:_simplifyHomePath {
     $pwd.Path -replace ([regex]::Escape($HOME)+'(.*)'),'~$1'
@@ -6,22 +6,12 @@ function global:_simplifyHomePath {
 
 if ($ripwsh.root) {
 function global:prompt {
-    Write-Host -NoNewline -ForegroundColor DarkGray "["
-    Write-Host -NoNewline -ForegroundColor Red "$([Environment]::UserName)"
-    Write-Host -NoNewline -ForegroundColor DarkGray "@"
-    Write-Host -NoNewline -ForegroundColor Red "$([Environment]::MachineName)"
-    Write-Host -NoNewline " "
     Write-Host -NoNewline -ForegroundColor Red "$(_simplifyHomePath)"
-    Write-Host -NoNewline -ForegroundColor DarkGray "]!>"
+    Write-Host -NoNewline -ForegroundColor DarkGray "!>"
     return " "
 }} else {
 function global:prompt {
-    Write-Host -NoNewline -ForegroundColor DarkGray "["
-    Write-Host -NoNewline -ForegroundColor Blue "$([Environment]::UserName)"
-    Write-Host -NoNewline -ForegroundColor DarkGray "@"
-    Write-Host -NoNewline -ForegroundColor Blue "$([Environment]::MachineName)"
-    Write-Host -NoNewline " "
     Write-Host -NoNewline -ForegroundColor Cyan "$(_simplifyHomePath)"
-    Write-Host -NoNewline -ForegroundColor DarkGray "]>"
+    Write-Host -NoNewline -ForegroundColor DarkGray ">"
     return " "
  }}
