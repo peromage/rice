91c2f1c396fa397b3d1e0e1c06a91376f11decac
peromage <peromage@outlook.com>
2022-06-10 15:12:08 -0400

    Add more extra packages

--- lisp/elpa-extra.el
+++ lisp/elpa-extra.el
@@ -1,7 +1,7 @@
 ;;; elpa-extra.el --- Extra interesting packages -*- lexical-binding: t -*-
 ;;; Commentary:
 ;; This collection is used to keep some interesting packages which however may
-;; not be used in every-day works.
+;; not be used in every-day work.
 
 ;;; Code:
 ;; Rainbow cat buffer progress bar
@@ -9,6 +9,12 @@
 
 ;; Colorful parenthesises
 (use-package rainbow-delimiters :defer t)
+
+;; Colorize color code
+(use-package rainbow-mode :defer t)
+
+;; Highlight current line
+(use-package beacon :defer t)
 
 (provide 'elpa-extra)
 ;;; elpa-extra.el ends here
