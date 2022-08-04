a1cf54002d10832b1201f84eaba38537ee04dc54
peromage <peromage@outlook.com>
2022-08-04 13:39:59 -0400

    rice: Add an option to force restart gpg-agent

--- rice/shell/bash/init-default.sh
+++ rice/shell/bash/init-default.sh
@@ -13,6 +13,10 @@ update-ssh-agent() {
 }
 
 update-gpg-agent() {
+    if [[ "x$1" = "x-f" ]]; then
+        echo "Restarting gpg-agent..."
+        gpgconf --kill gpg-agent
+    fi
     unset SSH_AGENT_PID
     export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
     export GPG_TTY="$(tty)"
