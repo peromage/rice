644c1afde33bbdfc142a1b045908a1302793e3f3
peromage <peromage@outlook.com>
2022-11-03 11:42:20 -0400

    Add a workaround to fix some shit

---
+++ workaround.el
@@ -0,0 +1,23 @@
+;;; workaround.el --- Additional workaround -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; This file contains temporary fixes for some configurations which do not take
+;; effect somehow.
+;; This should be loaded after init.el.
+;; Can be loaded by `(load (locate-user-emacs-file "workaround.el"))'
+
+;;; Code:
+
+(pew/config
+  :eval
+  (message "[pew] Loading workaround")
+  :custom
+  ;; LSP somehow loads the old version of clangd.
+  (lsp-clangd-version "15.0.3")
+  ;; These Evil settings somehow doesn't take effective from use-package.
+  (evil-want-Y-yank-to-eol t)
+  (evil-disable-insert-state-bindings t)
+  (evil-search-module 'evil-search))
+
+(provide 'workaround)
+;;; workaround.el ends here
