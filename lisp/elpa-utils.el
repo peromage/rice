74271657a5845a02d27d70eedd272c624cdbdd81
peromage <peromage@outlook.com>
2022-11-20 14:12:40 -0500

    pew/vterm/new now can specify shell to start with

--- lisp/elpa-utils.el
+++ lisp/elpa-utils.el
@@ -14,11 +14,16 @@
   :hook (vterm-mode . pew/terminal-common-setup)
 
   :init
-  (defun pew/vterm/new ()
-    "Create a new vterm window with a unique name."
-    (interactive)
-    (with-current-buffer (vterm)
-      (rename-buffer "*pewterm*" t)))
+  (defun pew/vterm/new (arg)
+    "Create a new vterm window.
+ARG is a prefix argument.  If it is non-nill, a prompt will pop up to allow
+users to specify the shell to start with."
+    (interactive "P")
+    (cond (arg
+           (let ((vterm-shell (read-string "Shell: ")))
+             (vterm :new)))
+          (t
+           (vterm :new))))
 
   :custom
   (vterm-kill-buffer-on-exit t)
