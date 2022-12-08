63d4d8b93fd2a1a2f461c8624bb5e05b6beaf9ff
peromage <peromage@outlook.com>
2022-12-07 20:50:51 -0500

    rice@bash: Support bash profile environment settings

--- rice/bash/install.sh
+++ rice/bash/install.sh
@@ -6,3 +6,8 @@ cat <<EOF >>$HOME/.bashrc
 source $THIS_DIR/init.sh
 reload-gpg-agent
 EOF
+
+cat <<EOF >>$HOME/.bash_profile
+## Add section names as arguments to enable their environment settings
+source $THIS_DIR/env.sh
+EOF
