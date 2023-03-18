15aa52b7eecfe35f6f421ee1a4c91259d95eb407
peromage <peromage@outlook.com>
2023-03-18 18:09:37 -0400

    rice@wzterm: Update config

--- rice/wezterm/wezterm.lua
+++ rice/wezterm/wezterm.lua
@@ -7,48 +7,50 @@ local my_keys = {
    { mods = "CTRL|SHIFT",  key = "Tab",    action = act.ActivateTabRelative(-1) },
    { mods = "CTRL|SHIFT",  key = "C",      action = act.CopyTo "Clipboard" },
    { mods = "CTRL|SHIFT",  key = "V",      action = act.PasteFrom "Clipboard" },
-   { mods = "CTRL|SHIFT",  key = "P",      action = act.ActivateCommandPalette },
+   { mods = "CTRL|SHIFT",  key = "S",      action = act.QuickSelect },
    { mods = "CTRL|SHIFT",  key = "U",      action = act.CharSelect{ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }},
+   -- Most of the less used commands can be accessed from the palette or the launcher
+   { mods = "CTRL|SHIFT",  key = "P",      action = act.ActivateCommandPalette },
    { mods = "CTRL|SHIFT",  key = "L",      action = act.ShowLauncher },
    { mods = "ALT",         key = "Enter",  action = act.ToggleFullScreen },
    -- Mode shift
-   { mods = "LEADER",      key = "Space",  action = act.ActivateCopyMode },
-   { mods = "LEADER",      key = "g",      action = act.ActivateKeyTable{ name = "my_transient_mode_table", one_shot = false }},
-   { mods = "LEADER",      key = "f",      action = act.Search "CurrentSelectionOrEmptyString" },
-   { mods = "LEADER",      key = "d",      action = act.ShowDebugOverlay },
-   { mods = "LEADER",      key = "s",      action = act.QuickSelect },
-   -- Tabs
-   { mods = "LEADER",      key = "t",      action = act.SpawnTab "CurrentPaneDomain" },
-   { mods = "LEADER",      key = "Q",      action = act.CloseCurrentTab{ confirm = true }},
-   -- Panes
-   { mods = "LEADER",      key = "2",      action = act.SplitVertical{ domain = "CurrentPaneDomain" }},
-   { mods = "LEADER",      key = "3",      action = act.SplitHorizontal{ domain = "CurrentPaneDomain" }},
-   { mods = "LEADER",      key = "z",      action = act.TogglePaneZoomState },
-   { mods = "LEADER",      key = "o",      action = act.PaneSelect{ mode =  "Activate" } },
-   { mods = "LEADER",      key = "q",      action = act.CloseCurrentPane{ confirm = true }},
-   -- Windows
-   { mods = "LEADER",      key = "N",      action = act.SpawnWindow },
-   -- Workspace
-   { mods = "LEADER",      key = "n",      action = act.SwitchWorkspaceRelative(1) },
-   { mods = "LEADER",      key = "p",      action = act.SwitchWorkspaceRelative(-1) },
+   { mods = "CTRL|SHIFT",  key = "Space",  action = act.ActivateCopyMode },
+   { mods = "CTRL|SHIFT",  key = "J",      action = act.ActivateKeyTable{ name = "my_transient_mode_table", one_shot = true, timeout_milliseconds = 1000 }},
+   { mods = "CTRL|SHIFT",  key = "K",      action = act.ActivateKeyTable{ name = "my_transient_mode_table", one_shot = false, timeout_milliseconds = 1000 }},
 }
-
 local my_transient_mode_table = {
+   -- Exit keys
+   { mods = "NONE",  key = "Escape",  action = act.PopKeyTable },
+   { mods = "CTRL",  key = "g",       action = act.PopKeyTable },
+   -- Tabs
+   { mods = "NONE",  key = "t",       action = act.SpawnTab "CurrentPaneDomain" },
+   { mods = "NONE",  key = "Q",       action = act.CloseCurrentTab{ confirm = true }},
+   -- Panes
+   { mods = "NONE",  key = "2",       action = act.SplitVertical{ domain = "CurrentPaneDomain" }},
+   { mods = "NONE",  key = "3",       action = act.SplitHorizontal{ domain = "CurrentPaneDomain" }},
+   { mods = "NONE",  key = "z",       action = act.TogglePaneZoomState },
+   { mods = "NONE",  key = "o",       action = act.PaneSelect{ mode =  "Activate" }},
+   { mods = "NONE",  key = "q",       action = act.CloseCurrentPane{ confirm = true }},
+   -- Windows
+   { mods = "NONE",  key = "N",       action = act.SpawnWindow },
+   -- Workspace
+   { mods = "NONE",  key = "n",       action = act.SwitchWorkspaceRelative(1) },
+   { mods = "NONE",  key = "p",       action = act.SwitchWorkspaceRelative(-1) },
+   -- Keys that are used repeatedly most of the time
    { mods = "CTRL",  key = "0",       action = act.ResetFontSize },
    { mods = "CTRL",  key = "=",       action = act.IncreaseFontSize },
    { mods = "CTRL",  key = "-",       action = act.DecreaseFontSize },
-   { mods = "CTRL",  key = "h",       action = act.AdjustPaneSize{ "Left", 1}},
-   { mods = "CTRL",  key = "j",       action = act.AdjustPaneSize{ "Down", 1}},
-   { mods = "CTRL",  key = "k",       action = act.AdjustPaneSize{ "Up", 1}},
-   { mods = "CTRL",  key = "l",       action = act.AdjustPaneSize{ "Right", 1}},
+   { mods = "CTRL",  key = "h",       action = act.AdjustPaneSize{ "Left",   1}},
+   { mods = "CTRL",  key = "j",       action = act.AdjustPaneSize{ "Down",   1}},
+   { mods = "CTRL",  key = "k",       action = act.AdjustPaneSize{ "Up",     1}},
+   { mods = "CTRL",  key = "l",       action = act.AdjustPaneSize{ "Right",  1}},
    { mods = "CTRL",  key = "o",       action = act.RotatePanes "Clockwise" },
-   { mods = "NONE",  key = "Escape",  action = act.PopKeyTable },
-   { mods = "CTRL",  key = "g",       action = act.PopKeyTable },
 }
 
 local my_copy_mode_table = {
    -- Exit
    { mods = "NONE",   key = "q",       action = act.CopyMode "Close" },
+   { mods = "CTRL" ,  key = "g",       action = act.CopyMode "Close" },
    -- Cursor movement
    { mods = "NONE",   key = "h",       action = act.CopyMode "MoveLeft" },
    { mods = "NONE",   key = "j",       action = act.CopyMode "MoveDown" },
@@ -75,14 +77,16 @@ local my_copy_mode_table = {
    { mods = "CTRL" ,  key = "v",       action = act.CopyMode{ SetSelectionMode = "Block" }},
    { mods = "SHIFT",  key = "V",       action = act.CopyMode{ SetSelectionMode = "Line" }},
    { mods = "NONE" ,  key = "y",       action = act.CopyTo "ClipboardAndPrimarySelection" },
+   { mods = "NONE" ,  key = "u",       action = act.CopyMode "ClearSelectionMode" },
    { mods = "NONE" ,  key = "Escape",  action = act.CopyMode "ClearSelectionMode" },
-   { mods = "CTRL" ,  key = "g",       action = act.CopyMode "ClearSelectionMode" },
 
    -- Search
    { mods = "NONE",   key = "f",       action = act.CopyMode{ JumpForward = { prev_char = false }}},
    { mods = "SHIFT",  key = "F",       action = act.CopyMode{ JumpBackward = { prev_char = false }}},
    { mods = "NONE",   key = "t",       action = act.CopyMode{ JumpForward = { prev_char = true }}},
    { mods = "SHIFT",  key = "T",       action = act.CopyMode{ JumpBackward = { prev_char = true }}},
+   -- Switch to the search mode directly since the "EditPattern" has conflicts
+   -- with the CopyMode at this moment (Hope it will be fixed in the future)
    { mods = "NONE",   key = "/",       action = act.Search "CurrentSelectionOrEmptyString" },
    { mods = "SHIFT",  key = "#",       action = act.CopyMode "ClearPattern" },
    { mods = "NONE",   key = "n",       action = act.CopyMode "NextMatch" },
@@ -92,20 +96,34 @@ local my_copy_mode_table = {
 }
 
 local my_search_mode_table = {
-   { mods = "NONE",   key = "Escape",  action = act.CopyMode "Close" },
-   { mods = "CTRL",   key = "g",       action = act.CopyMode "Close" },
-   { mods = "NONE",   key = "Enter",   action = act.ActivateCopyMode },
-   { mods = "CTRL",   key = "i",       action = act.CopyMode "CycleMatchType" },
-   { mods = "CTRL",   key = "s",       action = act.CopyMode "NextMatch" },
-   { mods = "CTRL",   key = "r",       action = act.CopyMode "PriorMatch" },
-   { mods = "CTRL",   key = "k",       action = act.CopyMode "ClearPattern" },
+   -- Switch back to the CopyMode when the search pattern is accepted or canceled
+   -- Avoid accidentally exiting the CopyMode
+   { mods = "NONE",  key = "Enter",   action = act.ActivateCopyMode },
+   { mods = "NONE",  key = "Escape",  action = act.ActivateCopyMode },
+   { mods = "CTRL",  key = "g",       action = act.ActivateCopyMode },
+   -- Emacs style
+   { mods = "CTRL",  key = "i",       action = act.CopyMode "CycleMatchType" },
+   { mods = "CTRL",  key = "s",       action = act.CopyMode "NextMatch" },
+   { mods = "CTRL",  key = "r",       action = act.CopyMode "PriorMatch" },
+   { mods = "CTRL",  key = "k",       action = act.CopyMode "ClearPattern" },
+}
+
+local my_launcher_menu = {
+   {
+      label = "Pwsh",
+      args = { "pwsh", "-NoLogo" },
+   },
+   {
+      label = "Bash",
+      args = { "bash", "-i" },
+   }
 }
 
 return {
    -- Window appearance
    enable_tab_bar = true,
    use_fancy_tab_bar = true,
-   hide_tab_bar_if_only_one_tab = false,
+   hide_tab_bar_if_only_one_tab = true,
    tab_max_width = 16,
 
    -- Theme
@@ -117,11 +135,13 @@ return {
    -- Use my own keybindings
    disable_default_key_bindings = true,
    key_map_preference = "Mapped",
-   leader = { mods = "CTRL", key = "`" },
+   -- leader = { mods = "CTRL", key = "`" },
    keys = my_keys,
    key_tables = {
       search_mode = my_search_mode_table,
       copy_mode = my_copy_mode_table,
       my_transient_mode_table = my_transient_mode_table,
    },
+   -- Launcher
+   launch_menu = my_launcher_menu,
 }
