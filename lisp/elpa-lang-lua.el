81dd309e4e19051e36ca93db2171207176d461ab
peromage <peromage@outlook.com>
2023-03-19 23:26:40 -0400

    Add Lua support

---
+++ lisp/elpa-lang-lua.el
@@ -0,0 +1,12 @@
+;;; elpa-lang-lua.el --- Lua mode -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Support for Lua development.
+
+;;; Code:
+(use-package lua-mode
+  :custom
+  (lua-indent-level 4))
+
+(provide 'elpa-lang-lua)
+;;; elpa-lang-lua.el ends here
