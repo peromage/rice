c1e3e6f30e992f1771d38e73f7836f8917bdeefd
peromage <peromage@outlook.com>
2023-03-20 15:24:25 -0400

    rice@pwsh: Make init clearer

--- rice/pwsh/init.ps1
+++ rice/pwsh/init.ps1
@@ -8,21 +8,9 @@ if ($PSVersionTable.PSVersion.Major -lt 7) {
 }
 
 ### Initialization
-## Rice variables
-$rice = @{
-    home = Get-Item $PSScriptRoot
-    privileged = $IsWindows ? ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) : ((id -u) -eq 0)
-    platform_windows = $IsWindows ? $true : $false
-}
+. $PSScriptRoot/modules/init-base.ps1
 
-## Return a string with dot sourcing syntax
-## Due to the limitation, sourcing has to be done in the global scope
-function rice_include {
-    param ($file)
-    return ". $(Join-Path $rice.home.FullName $file) $args"
-}
-
-## PSReadLine
+### PSReadLine
 &{
     $my_psreadline_options = @{
         EditMode = "Emacs"
@@ -47,10 +35,10 @@ $Env:EDITOR = "vim"
 
 ### Load modules
 &{
-    rice_include modules/init-alias.ps1
-    rice_include modules/init-hack.ps1
-    rice_include modules/theme-minimalist.ps1
+    rice_include_expr modules/init-alias.ps1
+    rice_include_expr modules/init-hack.ps1
+    rice_include_expr modules/theme-minimalist.ps1
     if ($rice.platform_windows) {
-        rice_include modules/init-alias-windows.ps1
+        rice_include_expr modules/init-alias-windows.ps1
     }
 } | ForEach-Object { Invoke-Expression $_ }
