9cdaf903f0163795b6a67b317d2b13e9cd0cd316
peromage <peromage@outlook.com>
2023-03-19 23:17:27 -0400

    rice@wezterm: Add install script

---
+++ rice/wezterm/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+THIS_DIR=$(cd "$(dirname $BASH_SOURCE)" && pwd)
+CONFIG_DIR="$HOME/.config/wezterm"
+
+mkdir -p $CONFIG_DIR
+cat <<EOF >>$CONFIG_DIR/wezterm.vim
+local conf = dofile("$THIS_DIR/init.lua")
+return conf:rice_merge({
+    -- Custom config goes here
+    -- default_prog = {"pwsh", "-NoLogo"}
+})
+EOF
