f439c032bc4da25d1435f34925b5086d96550992
peromage <peromage@outlook.com>
2023-03-21 13:29:25 -0400

    rice@wezterm: Split config

---
+++ rice/wezterm/librice/launch-menu.lua
@@ -0,0 +1,18 @@
+--- launch-menu.lua --- Launch menu -*- lua-indent-level: 4; -*-
+
+local ricemeta = require "librice.meta"
+
+local rice_launch_menu = ricemeta:rice_bind({
+    {
+        label = "Pwsh",
+        args = { "pwsh", "-NoLogo" },
+    },
+    {
+        label = "Bash",
+        args = { "bash", "-i" },
+    },
+})
+
+return {
+    launch_menu = rice_launch_menu,
+}
