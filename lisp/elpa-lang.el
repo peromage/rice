bc9f5f720986c647bd9cb96ed0f90feadd3d9804
peromage <peromage@outlook.com>
2023-04-11 09:38:56 -0400

    Add package cmake-mode

--- lisp/elpa-lang.el
+++ lisp/elpa-lang.el
@@ -6,8 +6,14 @@
 ;;; Code:
 ;;; No brainer modes
 (use-package vimrc-mode :defer t)
+
 (use-package yaml-mode :defer t)
+
 (use-package json-mode :defer t)
+
+(use-package cmake-mode :defer t
+  :custom
+  cmake-tab-width 4)
 
 (provide 'elpa-lang)
 ;;; elpa-lang.el ends here
