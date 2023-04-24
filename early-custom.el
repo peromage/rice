38b403dc5d415d63c083979ff54c2b71cfba0314
Fang Deng <fang@elfang.com>
2023-04-24 13:53:17 -0400

    Load workaround by default

---
+++ early-custom.el
@@ -0,0 +1,11 @@
+;;; early-custom.el --- Early load -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(require 'my-workaround)
+
+(provide 'early-custom)
+;;; early-custom.el ends here
--- site-lisp/my-workaround.el
+++ site-lisp/my-workaround.el
@@ -7,16 +7,14 @@
 ;; Can be loaded by `(load (locate-user-emacs-file "workaround.el"))'
 
 ;;; Code:
-(pewcfg
-  :eval
-  (message "[pew] Loading workaround")
-  :custom
-  ;; LSP somehow loads the old version of clangd.
-  (lsp-clangd-version "15.0.3")
-  ;; These Evil settings somehow doesn't take effective from use-package.
-  (evil-want-Y-yank-to-eol t)
-  (evil-disable-insert-state-bindings t)
-  (evil-search-module 'evil-search))
+;; LSP somehow loads the old version of clangd.
+(setq lsp-clangd-version "15.0.3")
+;; These Evil settings somehow doesn't take effective from use-package.
+(setq evil-want-Y-yank-to-eol t)
+(setq evil-disable-insert-state-bindings t)
+(setq evil-search-module 'evil-search)
+
+(message "[pew] Loaded my-workaround")
 
 (provide 'my-workaround)
 ;;; my-workaround.el ends here
