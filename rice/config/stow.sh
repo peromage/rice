03438b2507fabf5757d8a7098e5343e9baba9d3e
Fang Deng <fang@elfang.com>
2023-04-28 11:16:36 -0400

    rice: Improve preset usability

--- rice/config/stow.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-## A simple wrapper of stow
-
-cd $(realpath -s $(dirname $BASH_SOURCE))
-stow -t $HOME $@
--- rice/setup.sh
+++ rice/setup.sh
@@ -1,50 +1,62 @@
 #!/usr/bin/bash
 
 ### Variables and functions
-THIS_DIR=$(realpath -s $(dirname $BASH_SOURCE))
+THIS_DIR=$(dirname $(realpath $BASH_SOURCE))
+cd $THIS_DIR
 
 function setup_help {
     cat <<EOF
 $0 usage:
 $(declare -F | perl -ne '/setup_(.+)/ and print "  --$1\n"')
+
+Available presets:
+$(ls | perl -ne '/PRESET-(.+)/ and print "  $1\n"')
 EOF
 }
 
 function setup_sync_with_stash {
-    cd $THIS_DIR
-    [[ "No local changes to save" == $(git stash push) ]] && local pop_needed=1
+    [[ "No local changes to save" == $(git stash push) ]] || local pop_needed=1
     git pull
     [[ -n $pop_needed ]] && git stash pop
     if [[ -n "$(git diff --name-only --diff-filter=U)" ]]; then
         ## Conflicts
-        echo -e "\e[31;1m>> FAILED: Conflicts found after sync\e[0m"
+        echo -e "\e[31;1mConflicts found after sync\e[0m"
         exit 1
     fi
-    echo ">>  SUCCEEDED: sync_with_stash"
-    exit 0
 }
 
 function setup_sync {
-    cd $THIS_DIR
     git pull
-    echo ">>  SUCCEEDED: sync"
-    exit 0
 }
 
-function setup_stow {
+function prepare_dir {
     ## Make sure no accidental db submission
     mkdir $HOME/.gnupg 2>/dev/null && chmod 700 $HOME/.gnupg 2>/dev/null
     mkdir $HOME/bin 2>/dev/null
-    stow -d "$THIS_DIR/stow" -t $HOME .
+}
+
+function setup_stow {
+    prepare_dir
+    stow -d config -t $HOME .
 }
 
 function setup_unstow {
-    stow -d "$THIS_DIR/stow" -t $HOME -D .
+    stow -d config -t $HOME -D .
+}
+
+function setup_preset {
+    prepare_dir
+    ## Let errors be thrown by stow
+    stow -d "PRESET-$1" -t $HOME .
+}
+
+function setup_unpreset {
+    stow -d "PRESET-$1" -t $HOME -D .
 }
 
 ### Script starts here
 opt=${1:-} && shift
-if ! setup_help | grep -Pq -- "^ *$opt *$"; then
+if [[ -z "$opt" ]] || ! setup_help | grep -Pq -- "^ *$opt *$"; then
     setup_help
     printf "\nNot a valid option: $opt\n"
     exit 1
