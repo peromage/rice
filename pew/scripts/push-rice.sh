0104d6cfeaddaf2f8865a3b4b3d6178d540e7e7c
Fang Deng <fang@elfang.com>
2023-04-27 23:15:16 -0400

    Fix push-rice.sh

--- pew/scripts/push-rice.sh
+++ pew/scripts/push-rice.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
-cd "$(dirname $BASH_SOURCE)/.."
-
 TARGET_BRANCH=master
 
+if [[ ! -d rice ]]; then
+    echo "Not at the repo root directory!"
+    exit 1
+fi
+
 if [[ $(git branch --show-current) != $TARGET_BRANCH ]]; then
-    echo "Checkout master branch to proceed!"
+    echo "Checkout branch \"$TARGET_BRANCH\" to proceed!"
     exit 1
 fi
 
