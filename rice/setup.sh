2e98e54ed4746231b3a80abfe2d62621c970c132
Fang Deng <fang@elfang.com>
2023-04-28 23:03:34 -0400

    rice: Fix syntax issues for setup.sh

--- rice/setup.sh
+++ rice/setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
+set -e
 
 ### Variables and functions
-THIS_DIR=$(dirname $(realpath $BASH_SOURCE))
-cd $THIS_DIR
+THIS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+cd "$THIS_DIR"
 
 function setup_help {
     cat <<EOF
@@ -10,7 +11,7 @@ $0 usage:
 $(declare -F | perl -ne '/setup_(.+)/ and print "  --$1\n"')
 
 Available presets:
-$(ls | perl -ne '/PRESET-(.+)/ and print "  $1\n"')
+$(find ./* -type d -prune | perl -ne '/PRESET-(.+)/ and print "  $1\n"')
 EOF
 }
 
@@ -31,35 +32,36 @@ function setup_sync {
 
 function prepare_dir {
     ## Make sure no accidental db submission
-    mkdir $HOME/.gnupg 2>/dev/null && chmod 700 $HOME/.gnupg 2>/dev/null
-    mkdir $HOME/bin 2>/dev/null
+    mkdir "$HOME/.gnupg" 2>/dev/null && chmod 700 "$HOME/.gnupg" 2>/dev/null
+    mkdir "$HOME/bin" 2>/dev/null
 }
 
 function setup_stow {
     prepare_dir
-    stow -d config -t $HOME $@
+    stow -d config -t "$HOME" "$@"
 }
 
 function setup_unstow {
-    stow -d config -t $HOME -D $@
+    stow -d config -t "$HOME" -D "$@"
 }
 
 function setup_preset {
     prepare_dir
     ## Let errors be thrown by stow
-    stow -d "PRESET-$1" -t $HOME .
+    stow -d "PRESET-$1" -t "$HOME" .
 }
 
 function setup_unpreset {
-    stow -d "PRESET-$1" -t $HOME -D .
+    stow -d "PRESET-$1" -t "$HOME" -D .
 }
 
 ### Script starts here
-opt=${1:-} && shift
+opt="${1:-}"
+shift || true ## Ignore 0 argument shifting
 if [[ -z "$opt" ]] || ! setup_help | grep -Pq -- "^ *$opt *$"; then
     setup_help
-    printf "\nNot a valid option: $opt\n"
+    printf "\n%s\n" "Not a valid option: $opt"
     exit 1
 fi
 
-setup_${opt#--} $@
+"setup_${opt#--}" "$@"
