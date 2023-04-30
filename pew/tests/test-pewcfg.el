3f31bb1d13e72ce2169cf353d8d76f927b7c40de
Fang Deng <fang@elfang.com>
2023-04-30 16:23:16 -0400

    Reformat pewcfg test helpers

--- pew/tests/test-pewcfg.el
+++ pew/tests/test-pewcfg.el
@@ -7,164 +7,153 @@
 ;;; Code:
 (progn
 ;;; Test helpers
-  (defun expect (name a b)
+  (defun expect-equal (name a b)
     "Compare A and B and emit error if they don't match.
 NAME is used to identify the name of this comparison."
+    (declare (indent 1))
     (if (equal a b)
         (message "[ PASSED ] %s" name)
       (error "[ FAILED ] %s" name)))
 
-  (defmacro expect-expansion (name step macro expection)
-    "Expand MACRO and compare the result with EXPECTION.
+  (defmacro expect-expansion (name step expectation macro)
+    "Expand MACRO and compare the result with EXPECTATION.
 NAME is the name of thie comparison.
 STEP defines how deep the macro will be expanded.  The value is the same with
 the one passed to `pew::expand-macro'."
-    `(expect ,name
-             (pew::expand-macro ,macro ,step :result)
-             ,expection))
-
+    (declare (indent 2))
+    `(expect-equal ,name
+       ,expectation
+       (pew::expand-macro ,macro ,step :result)))
 
 ;;; Tests start
   (message "[ BEGIN ] Testing for pewcfg")
 
-  (expect-expansion
-   "set-custom" :all
-   (pewcfg::set-custom foo foovalue "comment")
-   '(customize-set-variable 'foo foovalue "comment"))
+;;; :custom
+  (expect-expansion "set-custom" :all
+    '(customize-set-variable 'foo foovalue "comment")
+    (pewcfg::set-custom foo foovalue "comment"))
 
-  (expect-expansion
-   "set-custom: No comment" :all
-   (pewcfg::set-custom foo foovalue)
-   '(customize-set-variable 'foo foovalue nil))
+  (expect-expansion "set-custom: No comment" :all
+    '(customize-set-variable 'foo foovalue nil)
+    (pewcfg::set-custom foo foovalue))
 
-  (expect-expansion
-   "set-bind" :all
-   (pewcfg::set-bind foo-map
+;;; :bind
+  (expect-expansion "set-bind" :all
+    '(progn (define-key foo-map "a" #'func1)
+            (define-key foo-map "b" #'func2)
+            foo-map)
+    (pewcfg::set-bind foo-map
+                      ("a" . func1)
+                      ("b" . func2)))
+
+  (expect-expansion "set-bind: No definitions" :all
+    '(progn foo-map)
+    (pewcfg::set-bind foo-map))
+
+;;; :map
+  (expect-expansion "set-map" 1
+    '(progn (define-prefix-command 'foo-map)
+            (pewcfg::set-bind foo-map
+                              ("a" . func1)
+                              ("b" . func2)))
+    (pewcfg::set-map foo-map
                      ("a" . func1)
-                     ("b" . func2))
-   '(progn (define-key foo-map "a" #'func1)
-           (define-key foo-map "b" #'func2)
-           foo-map))
+                     ("b" . func2)))
 
-  (expect-expansion
-   "set-bind: No definitions" :all
-   (pewcfg::set-bind foo-map)
-   '(progn foo-map))
+  (expect-expansion "set-map: No definitions" 1
+    '(progn (define-prefix-command 'foo-map)
+            (pewcfg::set-bind foo-map))
+    (pewcfg::set-map foo-map))
 
-  (expect-expansion
-   "set-map" 1
-   (pewcfg::set-map foo-map
-                    ("a" . func1)
-                    ("b" . func2))
-   '(progn (define-prefix-command 'foo-map)
-           (pewcfg::set-bind foo-map
-                             ("a" . func1)
-                             ("b" . func2))))
+;;; :transient
+  (expect-equal "set-transient"
+    '(progn (pewcfg::set-map foo-map ("a" . func1) ("b" . func2))
+            (define-key foo-map (kbd "C-h") (lambda () (interactive) (foo :repeat)))
+            (define-key foo-map (kbd "C-g") #'keyboard-quit)
+            (defun foo (arg)))
+    (let ((l:expansion (pew::expand-macro (pewcfg::set-transient foo
+                                                                 ("a" . func1)
+                                                                 ("b" . func2))
+                                          1 t)))
+      ;; Ignore the function definition
+      (setf (nth 4 l:expansion) (seq-take (nth 4 l:expansion) 3))
+      l:expansion))
 
-  (expect-expansion
-   "set-map: No definitions" 1
-   (pewcfg::set-map foo-map)
-   '(progn (define-prefix-command 'foo-map)
-           (pewcfg::set-bind foo-map)))
+  (expect-equal "set-transient: No definitions"
+    '(progn (pewcfg::set-map foo-map)
+            (define-key foo-map (kbd "C-h") (lambda () (interactive) (foo :repeat)))
+            (define-key foo-map (kbd "C-g") #'keyboard-quit)
+            (defun foo (arg)))
+    (let ((l:expansion (pew::expand-macro (pewcfg::set-transient foo)
+                                          1 t)))
+      ;; Ignore the function definition
+      (setf (nth 4 l:expansion) (seq-take (nth 4 l:expansion) 3))
+      l:expansion))
 
-  (expect
-   "set-transient"
-   (let ((l:expansion (pew::expand-macro (pewcfg::set-transient foo
-                                                                ("a" . func1)
-                                                                ("b" . func2))
-                                         1 t)))
-     ;; Ignore the function definition
-     (setf (nth 4 l:expansion) (seq-take (nth 4 l:expansion) 3))
-     l:expansion)
-   '(progn (pewcfg::set-map foo-map ("a" . func1) ("b" . func2))
-           (define-key foo-map (kbd "C-h") (lambda () (interactive) (foo :repeat)))
-           (define-key foo-map (kbd "C-g") #'keyboard-quit)
-           (defun foo (arg))))
+;;; :switch
+  (expect-equal "set-switch"
+    '(progn (defvar switch/foo '(-1 v1 v2 v3))
+            (defun switch/foo ()))
+    (let ((l:expansion (pew::expand-macro (pewcfg::set-switch foo (v1 v2 v3))
+                                          1 t)))
+      ;; Ignore the variable comment
+      (setf (nth 1 l:expansion) (butlast (nth 1 l:expansion)))
+      ;; Ignore the function definition
+      (setf (nth 2 l:expansion) (seq-take (nth 2 l:expansion) 3))
+      l:expansion))
 
-  (expect
-   "set-transient: No definitions"
-   (let ((l:expansion (pew::expand-macro (pewcfg::set-transient foo)
-                                         1 t)))
-     ;; Ignore the function definition
-     (setf (nth 4 l:expansion) (seq-take (nth 4 l:expansion) 3))
-     l:expansion)
-   '(progn (pewcfg::set-map foo-map)
-           (define-key foo-map (kbd "C-h") (lambda () (interactive) (foo :repeat)))
-           (define-key foo-map (kbd "C-g") #'keyboard-quit)
-           (defun foo (arg))))
+  (expect-equal "set-switch: Default values"
+    '(progn (defvar switch/foo '(-1 nil t))
+            (defun switch/foo ()))
+    (let ((l:expansion (pew::expand-macro (pewcfg::set-switch foo)
+                                          1 t)))
+      ;; Ignore the variable comment
+      (setf (nth 1 l:expansion) (butlast (nth 1 l:expansion)))
+      ;; Ignore the function definition
+      (setf (nth 2 l:expansion) (seq-take (nth 2 l:expansion) 3))
+      l:expansion))
 
-  (expect
-   "set-switch"
-   (let ((l:expansion (pew::expand-macro (pewcfg::set-switch foo
-                                                             (v1 v2 v3))
-                                         1 t)))
-     ;; Ignore the variable comment
-     (setf (nth 1 l:expansion) (butlast (nth 1 l:expansion)))
-     ;; Ignore the function definition
-     (setf (nth 2 l:expansion) (seq-take (nth 2 l:expansion) 3))
-     l:expansion)
-   '(progn (defvar switch/foo '(-1 v1 v2 v3))
-           (defun switch/foo ())))
+;;; :face
+  (expect-expansion "set-face" :all
+    '(set-face-attribute 'foo nil
+                         :family "bar"
+                         :weight 'normal
+                         :height 120
+                         :width 'normal)
+    (pewcfg::set-face foo
+                      :family "bar"
+                      :weight normal
+                      :height 120
+                      :width normal))
 
-  (expect
-   "set-switch: Default values"
-   (let ((l:expansion (pew::expand-macro (pewcfg::set-switch foo)
-                                         1 t)))
-     ;; Ignore the variable comment
-     (setf (nth 1 l:expansion) (butlast (nth 1 l:expansion)))
-     ;; Ignore the function definition
-     (setf (nth 2 l:expansion) (seq-take (nth 2 l:expansion) 3))
-     l:expansion)
-   '(progn (defvar switch/foo '(-1 nil t))
-           (defun switch/foo ())))
+  (expect-expansion "set-property" :all
+    '(progn (put 'foo 'p1 v1)
+            (put 'foo 'p2 v2))
+    (pewcfg::set-property foo (p1 . v1) (p2 . v2)))
 
-  (expect-expansion
-   "set-face" :all
-   (pewcfg::set-face foo
-                     :family "bar"
-                     :weight normal
-                     :height 120
-                     :width normal)
-   '(set-face-attribute 'foo nil
-                        :family "bar"
-                        :weight 'normal
-                        :height 120
-                        :width 'normal))
+  (expect-expansion "set-hook" :all
+    '(add-hook 'foo-hook #'func)
+    (pewcfg::set-hook foo func))
 
-  (expect-expansion
-   "set-property" :all
-   (pewcfg::set-property foo (p1 . v1) (p2 . v2))
-   '(progn (put 'foo 'p1 v1)
-           (put 'foo 'p2 v2)))
+  (expect-expansion "set-automode" :all
+    '(add-to-list 'auto-mode-alist '("matcher regex" . foo-mode))
+    (pewcfg::set-automode "matcher regex" foo-mode))
 
-  (expect-expansion
-   "set-hook" :all
-   (pewcfg::set-hook foo func)
-   '(add-hook 'foo-hook #'func))
+  (expect-expansion "set-eval" :all
+    '(foo bar)
+    (pewcfg::set-eval (foo bar)))
 
-  (expect-expansion
-   "set-automode" :all
-   (pewcfg::set-automode "matcher regex" foo-mode)
-   '(add-to-list 'auto-mode-alist '("matcher regex" . foo-mode)))
+  (expect-expansion "set-eval-after" 1
+    '(with-eval-after-load 'foo (call 911) (call another))
+    (pewcfg::set-eval-after foo (call 911) (call another)))
 
-  (expect-expansion
-   "set-eval" :all
-   (pewcfg::set-eval (foo bar))
-   '(foo bar))
+;;; Utilities
+  (expect "tokey: string"
+    ""
+    (pewcfg::tokey "C-c C-c"))
 
-  (expect-expansion
-   "set-eval-after" 1
-   (pewcfg::set-eval-after foo (call 911) (call another))
-   '(with-eval-after-load 'foo (call 911) (call another)))
-
-  (expect
-   "Tokey key string"
-   (pewcfg::tokey "C-c C-c")
-   "")
-
-  (expect
-   "Tokey key vector"
-   (pewcfg::tokey [tab])
-   [tab])
+  (expect "tokey: vector"
+    [tab]
+    (pewcfg::tokey [tab]))
 
   (message "[ END ] Passed all"))
