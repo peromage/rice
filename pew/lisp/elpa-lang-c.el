685033e879e69aa3978c19101ab9bf82838f599e
Fang Deng <fang@elfang.com>
2023-05-31 16:56:11 -0400

    Update CC mode config

--- pew/lisp/elpa-lang-c.el
+++ pew/lisp/elpa-lang-c.el
@@ -14,15 +14,21 @@
     "Common CC mode setup."
     (c-set-offset 'substatement-open 0)
     (c-set-offset 'innamespace 0)
+    ;; Indentation
+    (setq-local indent-tabs-mode nil)
     (setq-local c++-tab-always-indent t)
     (setq-local c-basic-offset 4)
-    ;;(setq-local c-syntactic-indentation nil)
-    ;;(setq-local c-syntactic-indentation-in-macros nil)
     (setq-local c-indent-level 4)
     (setq-local tab-width 4)
     (setq-local tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
-    (setq-local indent-tabs-mode nil)
-    (setq-local adaptive-fill-mode nil))
+    (setq-local c-syntactic-indentation t)
+    (setq-local c-syntactic-indentation-in-macros t)
+    ;; Fill columns
+    (setq-local adaptive-fill-mode nil)
+    ;; Macro line continuation
+    (setq-local c-backslash-column 80)
+    (setq-local c-backslash-max-column 160)
+    (setq-local c-auto-align-backslashes t))
 
   (defun pew::cc-mode::lsp-setup ()
     "LSP setup for CC mode."
