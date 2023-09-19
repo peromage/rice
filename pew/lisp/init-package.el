53f52f6b6fee945ded3087fff6432aa7a4172c54
Fang Deng <fang@elfang.com>
2023-09-19 15:44:02 -0400

    Add a use-package helper function

--- pew/lisp/init-package.el
+++ pew/lisp/init-package.el
@@ -3,9 +3,10 @@
 ;; This ELPA initialization configuration should be loaded as early as possible.
 ;;; Code:
 
-;;; Emacs builtin package manager
+;;; Builtin package manager
 (require 'package)
 
+;; Config
 (setq
  ;; Package contents
  package-enable-at-startup nil
@@ -25,13 +26,11 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;;; Use `use-package' to simplify package configuration
-;; Install `use-package'
+;;; `use-package'
 (eval-and-compile
   (unless (package-installed-p 'use-package)
     (package-install 'use-package))
-  (require 'use-package)
-  (message "[pew] Loaded use-package"))
+  (require 'use-package))
 
 ;; Default `use-package' behaviors
 (setq use-package-always-ensure t
@@ -40,6 +39,16 @@
       use-package-always-pin nil
       use-package-compute-statistics nil
       use-package-verbose nil)
+
+;; A convenient function to enable custom values from the synthetic theme
+(defun use-package--enable-custom-theme ()
+  "A quick way to enable all the settings from the `use-package' theme.
+After enabling, remove the synthetic theme from the enabled themes, so iterating
+over them to disable-all-themes won't disable it."
+  (interactive)
+  (let ((theme 'use-package))
+    (enable-theme theme)
+    (setq custom-enabled-themes (remq theme custom-enabled-themes))))
 
 (provide 'init-package)
 ;;; init-package.el ends here
