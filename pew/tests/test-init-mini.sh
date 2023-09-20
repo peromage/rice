605245eecf509402c10f2ebc0980d43264137ce7
Fang Deng <fang@elfang.com>
2023-09-20 16:41:45 -0400

    Add a test for mini init

---
+++ pew/tests/test-init-mini.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S emacs --batch --script
+;;; test-init-mini.sh --- Test minimal startup -*- mode: emacs-lisp; lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+;;; Process arguments
+(if (< (length argv) 1) (error "Not enough arguments"))
+(setq repo-root-path (nth 0 argv))
+
+;;; Test starts
+(require 'url-vars)
+(let* ((debug-on-error t)
+       (url-show-status nil)
+       (user-emacs-directory repo-root-path)
+       (user-init-file (expand-file-name "init-mini.el" user-emacs-directory))
+       (load-path (delq user-emacs-directory load-path)))
+  (load-file user-init-file)
+  (run-hooks 'after-init-hook)
+  (run-hooks 'emacs-startup-hook)
+  (message "%s started in %s" (emacs-version) (emacs-init-time)))
