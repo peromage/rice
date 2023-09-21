cdf94572a53286bbced218d3fd5b2e973fe8b560
Fang Deng <fang@elfang.com>
2023-09-21 18:41:35 -0400

    Add a script to convert pictures to favicons

---
+++ rice/bin/convert-favicon.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## Requires `imagemagick'
+
+if [ $# -lt 2 ]; then
+    echo "$0 <INPUT> <OUTPUT> [-remove-bg]"
+    exit 1
+fi
+
+input="$1" && shift
+output="$1" && shift
+if [ "-remove-bg" == "$1" ]; then
+    input_args="-background transparent"
+fi
+output_args="-define icon:auto-resize=16,24,32,48,64,72,96,128,256"
+convert $input_args "$input" $output_args "$output"
