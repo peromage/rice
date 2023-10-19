4d5b9a11c571530aaffe429065b4f7f7138d04cb
Fang Deng <fang@elfang.com>
2023-10-19 12:43:52 -0400

    rice@wezterm: Adjust indentation to 2 spaces

--- rice/config/wezterm/.config/wezterm/librice/.dir-locals.el
+++ rice/config/wezterm/.config/wezterm/librice/.dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((lua-mode . ((lua-indent-level . 4)
+((lua-mode . ((lua-indent-level . 2)
               (outline-regexp . "---\\(-* \\)"))))
