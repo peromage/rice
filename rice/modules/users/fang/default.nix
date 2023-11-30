1da4920fadbb630bd0926b93997cbc4e9471099d
Fang Deng <fang@elfang.com>
2023-11-30 16:27:54 -0500

    rice: Add myself back to wheel

--- rice/modules/users/fang/default.nix
+++ rice/modules/users/fang/default.nix
@@ -2,7 +2,7 @@
   rice.users.users = {
     fang = {
       id = 1001;
-      groups = [ "users" "audio" "video" "cdrom" "networkmanager" ];
+      groups = [ "wheel" "users" "audio" "video" "cdrom" "networkmanager" ];
     };
   };
 }
