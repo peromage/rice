654e42f108d534bd476eaeda44f56ebbf48a9977
Fang Deng <fang@elfang.com>
2023-12-07 23:19:21 -0500

    rice: Update home packages

--- rice/modules/hosts/potpie/packages.nix
+++ rice/modules/hosts/potpie/packages.nix
@@ -12,17 +12,39 @@
     coreutils
     pinentry
     gnupg
-    unzip
     tmux
+    zstd
+    tree
+    pinentry
+    gnupg
 
-    ## File system
+    ## Archive
+    zip
+    unzip
+    xz
+    p7zip
+
+    ## Filesystem
     ntfs3g
     exfat
     exfatprogs
     e2fsprogs
+    fuse
+    fuse3
 
-    ## Disk
+    ## Monitors
+    htop # CPU and memory
+    iotop # IO
+    iftop # Network
+
+    ## Devices
     parted
+    pciutils # lspci
+    usbutils # lsusb
+    lsof # List open files
+    dnsutils  # dig, nslookup
+    iperf3
+    nmap
 
     ## Apps
     appimage-run
