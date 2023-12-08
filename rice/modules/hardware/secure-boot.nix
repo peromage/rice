e525b9ea94dec701f29c2b127019de6df4b806ff
Fang Deng <fang@elfang.com>
2023-12-08 11:21:58 -0500

    rice: Commentary update

--- rice/modules/hardware/secure-boot.nix
+++ rice/modules/hardware/secure-boot.nix
@@ -1,14 +1,15 @@
 ### Sucure boot support
 
-## Information is from https://nixos.wiki/wiki/Secure_Boot
-##
-## Simplified steps:
-## 1. Run: sudo nix run nixpkgs#sbctl create-keys
-## 2. Import this module, build and reboot.
-## 3. Run: sudo nix run nixpkgs#sbctl verify
-## 4. Reboot and set UEFI in setup mode.
-## 5. Run: sudo nix run nixpkgs#sbctl enroll-keys -- --microsoft
-## 6. Run: bootctl status
+/* Information is from https://nixos.wiki/wiki/Secure_Boot
+
+   Simplified steps:
+   1. Run: sudo nix run nixpkgs#sbctl create-keys
+   2. Import this module, build and reboot.
+   3. Run: sudo nix run nixpkgs#sbctl verify
+   4. Reboot and set UEFI in setup mode.
+   5. Run: sudo nix run nixpkgs#sbctl enroll-keys -- --microsoft
+   6. Run: bootctl status
+*/
 
 { config, pkgs, lib, rice, ... }:
 
