0d88615ebc7300cc935a7de5cc02add7f828375c
Fang Deng <fang@elfang.com>
2023-12-07 19:49:27 -0500

    rice: Comment indentation

--- rice/flake.nix
+++ rice/flake.nix
@@ -68,23 +68,24 @@
 
       /* Notice that there is a minor difference between `packages' and `legacyPackages'.
 
-        From: https://github.com/NixOS/nixpkgs/blob/b2e41a5bd20d4114f27fe8d96e84db06b841d035/flake.nix#L47
+         From: https://github.com/NixOS/nixpkgs/blob/b2e41a5bd20d4114f27fe8d96e84db06b841d035/flake.nix#L47
 
-        The "legacy" in `legacyPackages` doesn't imply that the packages exposed
-        through this attribute are "legacy" packages. Instead, `legacyPackages`
-        is used here as a substitute attribute name for `packages`. The problem
-        with `packages` is that it makes operations like `nix flake show
-        nixpkgs` unusably slow due to the sheer number of packages the Nix CLI
-        needs to evaluate. But when the Nix CLI sees a `legacyPackages`
-        attribute it displays `omitted` instead of evaluating all packages,
-        which keeps `nix flake show` on Nixpkgs reasonably fast, though less
-        information rich.
-        */
+         The "legacy" in `legacyPackages` doesn't imply that the packages exposed
+         through this attribute are "legacy" packages. Instead, `legacyPackages`
+         is used here as a substitute attribute name for `packages`. The problem
+         with `packages` is that it makes operations like `nix flake show
+         nixpkgs` unusably slow due to the sheer number of packages the Nix CLI
+         needs to evaluate. But when the Nix CLI sees a `legacyPackages`
+         attribute it displays `omitted` instead of evaluating all packages,
+         which keeps `nix flake show` on Nixpkgs reasonably fast, though less
+         information rich.
+      */
 
       /* Via: `nix build .#PACKAGE_NAME', `nix shell', etc. */
       packages = callWithRice ./packages;
 
       /* Via: `nix fmt'
+
          Other options beside `alejandra' include `nixpkgs-fmt'
       */
       formatter = forSupportedSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
