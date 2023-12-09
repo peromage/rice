5654d6bbc51caea91ca5609bb05a9c5d2041a596
Fang Deng <fang@elfang.com>
2023-12-08 22:50:57 -0500

    rice: Address a gpg-agent issue when forwarding for SSH

--- rice/modules/homes/fang/apps/gpg.nix
+++ rice/modules/homes/fang/apps/gpg.nix
@@ -17,11 +17,18 @@ in {
     enableFishIntegration = true;
   };
 
+  ## Workaround to prevent SSH_AUTH_SOCK being set with wrong value
+  ## Ref: https://wiki.archlinux.org/title/GNOME/Keyring#Disabling
+  xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
+[Desktop Entry]
+Name=SSH Key Agent
+Type=Application
+Hidden=true
+'';
+
   ## Override with my own settings
-  home.file = {
-    ".gnupg" = {
-      source = "${src}/.gnupg";
-      recursive = true;
-    };
+  home.file.".gnupg" = {
+    source = "${src}/.gnupg";
+    recursive = true;
   };
 }
