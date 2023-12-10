6288ed536cea7d624713c70b0850d2224160045a
Fang Deng <fang@elfang.com>
2023-12-10 11:36:04 -0500

    rice: Resolve recursion issue when evaluating IME module

--- rice/modules/services/ime.nix
+++ rice/modules/services/ime.nix
@@ -1,42 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, rice, ... }:
 
 let
+  librice = rice.lib;
   cfg = config.rice.services.ime;
-
-  frontends = {
-    "fcitx" = {
-      i18n.inputMethod = {
-        enabled = "fcitx5";
-        fcitx5.addons = with pkgs; [
-          fcitx5-rime
-          fcitx5-configtool
-          fcitx5-chinese-addons
-          fcitx5-gtk
-        ];
-      };
-
-      environment.systemPackages = with pkgs; [
-        librime
-        rime-cli
-        rime-data
-      ];
-    };
-
-    "ibus" = {
-      i18n.inputMethod = {
-        enabled = "ibus";
-        ibus.engines = with pkgs; [
-          ibus-engines.rime
-        ];
-      };
-
-      environment.systemPackages = with pkgs; [
-        librime
-        rime-cli
-        rime-data
-      ];
-    };
-  };
 
 in with lib; {
   options.rice.services.ime = {
@@ -47,7 +13,44 @@ in with lib; {
     };
   };
 
-  config = let
-    enabled = optionalAttrs (frontends ? cfg.enabled) frontends.${cfg.enabled};
-  in enabled;
+  config = librice.mkMergeIf [
+    {
+      cond = "ibus" == cfg.enabled;
+      as = {
+        i18n.inputMethod = {
+          enabled = "ibus";
+          ibus.engines = with pkgs; [
+            ibus-engines.rime
+          ];
+        };
+
+        environment.systemPackages = with pkgs; [
+          librime
+          rime-cli
+          rime-data
+        ];
+      };
+    }
+
+    {
+      cond = "fcitx" == cfg.enabled;
+      as = {
+        i18n.inputMethod = {
+          enabled = "fcitx5";
+          fcitx5.addons = with pkgs; [
+            fcitx5-rime
+            fcitx5-configtool
+            fcitx5-chinese-addons
+            fcitx5-gtk
+          ];
+        };
+
+        environment.systemPackages = with pkgs; [
+          librime
+          rime-cli
+          rime-data
+        ];
+      };
+    }
+  ];
 }
