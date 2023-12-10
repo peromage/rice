1c7c934c6531f0134ac6747dd3b6340ff183dcba
Fang Deng <fang@elfang.com>
2023-12-10 13:17:15 -0500

    rice: Update password store settings

--- rice/modules/homes/fang/apps/password-store.nix
+++ rice/modules/homes/fang/apps/password-store.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.password-store = {
     enable = true;
+    settings = {
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
+      PASSWORD_STORE_CLIP_TIME = "30";
+    };
     package = pkgs.pass.withExtensions (exts: with exts; [
       pass-otp
       pass-genphrase
