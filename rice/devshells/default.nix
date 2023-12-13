1b5a7ca08755c3330d95af45856db439b885237e
Fang Deng <fang@elfang.com>
2023-12-12 19:35:20 -0500

    rice: Modularize devshells

--- rice/devshells/default.nix
+++ rice/devshells/default.nix
@@ -1,16 +1,15 @@
 { nixpkgs, outputs, rice, ... }:
 
 let
+  lib = nixpkgs.lib;
   librice = rice.lib;
 
-  mkDevshells = pkgs: {
-    ## Example
-    default = pkgs.mkShell {
-      packages = with pkgs.pkgsCustom; [
-        hello
-      ];
-    };
-  };
+  mkDevshells =
+    let allPackages = with librice; importAllAsAttrs (allButDefault ./.);
+    in pkgs: with lib; mapAttrs'
+      ## Strip extensions from names
+      (n: v: nameValuePair (librice.baseNameNoExt n) (pkgs.callPackage v {}))
+      allPackages;
 
 in with librice; forSupportedSystems (system:
   mkDevshells (import nixpkgs {
