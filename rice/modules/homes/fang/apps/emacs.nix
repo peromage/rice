d29e8cea90085f168ffecc9717ff2c29b72acd72
Fang Deng <fang@elfang.com>
2023-12-12 23:44:28 -0500

    rice: Update Emacs config

--- rice/modules/homes/fang/apps/emacs.nix
+++ rice/modules/homes/fang/apps/emacs.nix
@@ -1,19 +1,19 @@
 { pkgs, ... }:
 
 let
-  useEmacs = with pkgs; (emacsPackagesFor emacs29).emacsWithPackages;
+  useEmacsWith = with pkgs; (emacsPackagesFor emacs29).emacsWithPackages;
+
+  /* Refs:
+     https://github.com/martinbaillie/dotfiles/blob/c31b8aa815a940c45210c7ab9029141a8e6c6e93/modules/editors/emacs.nix#L30
+     https://nixos.wiki/wiki/Emacs
+  */
+  emacsPackages = epkgs: with epkgs; [
+    vterm # Since vterm cannot be compiled in user environment, use this instead
+  ];
 
 in {
   home.packages = with pkgs; [
-    /* Refs:
-       https://github.com/martinbaillie/dotfiles/blob/c31b8aa815a940c45210c7ab9029141a8e6c6e93/modules/editors/emacs.nix#L30
-       https://nixos.wiki/wiki/Emacs
-    */
-
-    (useEmacs (epkgs: with epkgs; [
-      vterm # Since vterm cannot be compiled in user environment, use this instead
-    ]))
-
+    (useEmacsWith emacsPackages)
     libvterm-neovim # libvterm is not maintained, use this instead
   ];
 }
