c7d44ae35e38ff6f7acd3d8c797e593bc9340705
Fang Deng <fang@elfang.com>
2023-12-14 13:29:33 -0500

    rice: Use MATE desktop environment for Chicken65

--- rice/modules/instances/Chicken65/configuration.nix
+++ rice/modules/instances/Chicken65/configuration.nix
@@ -52,8 +52,10 @@ in {
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.mate.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
