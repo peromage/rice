aa0e80897c6b5fe6de998dd6d500e1e294b556fc
Fang Deng <fang@elfang.com>
2023-12-14 16:11:37 -0500

    rice: Update powershell config

---
+++ rice/dotfiles/pwsh/.config/powershell/librice/my-prompt/my-prompt.psm1
@@ -0,0 +1,21 @@
+### my-prompt.psm1 --- My prompt style
+
+$isPrivileged = $IsWindows ? ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) : ((id -u) -eq 0)
+
+function simplify_home_path {
+    <# Replace home prefix with "~" #>
+    $pwd.Path -replace ([regex]::Escape($HOME)+'(.*)'),'~$1'
+}
+
+$function:prompt = {
+    if ($isPrivileged) {
+        Write-Host -NoNewline -ForegroundColor Red "$(simplify_home_path)"
+        Write-Host -NoNewline -ForegroundColor DarkGray "!>"
+        return " "
+    }
+    Write-Host -NoNewline -ForegroundColor Cyan "$(simplify_home_path)"
+    Write-Host -NoNewline -ForegroundColor DarkGray ">"
+    return " "
+}
+
+Export-ModuleMember -Function prompt
--- rice/dotfiles/pwsh/.config/powershell/profile.ps1
+++ rice/dotfiles/pwsh/.config/powershell/profile.ps1
@@ -1,13 +1,20 @@
 ### profile.ps1  -- Pwsh init -*- outline-regexp: "###\\(#* [^ \t\n]\\)"; -*-
 
-### Prerequisites ##############################################################
+### Prerequisites
 ## PowerShell 7 is required
 if ($PSVersionTable.PSVersion.Major -lt 7) {
     Write-Host -ForegroundColor Red "PowerShell 7 and above required"
     return
 }
 
-### Pwsh config ################################################################
+### Initialization
+## Global variables
+$RICE = @{}
+$RICE.root_dir = Get-Item "$PSScriptRoot"
+$RICE.overlay = (Join-Path $RICE.root_dir "profile-overlay.ps1")
+
+### Set it up
+## Readline settings
 &{
     $my_psreadline_options = @{
         EditMode = "Emacs"
@@ -21,42 +28,10 @@ if ($PSVersionTable.PSVersion.Major -lt 7) {
     Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 }
 
-### Environment variables ######################################################
-$RICE = @{}
-$RICE.root_dir = Get-Item "$PSScriptRoot"
-$RICE.overlay = (Join-Path $RICE.root_dir "profile-overlay.ps1")
-$RICE.privileged = $IsWindows ? ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) : ((id -u) -eq 0)
-## Modules will be autoloaded
+## Modules
 $Env:PSModulePath += [IO.Path]::PathSeparator + (Join-Path $RICE.root_dir librice)
-
-$function:prompt = {
-    if ($RICE.privileged) {
-        Write-Host -NoNewline -ForegroundColor Red "$(simplify_home_path)"
-        Write-Host -NoNewline -ForegroundColor DarkGray "!>"
-        return " "
-    }
-    Write-Host -NoNewline -ForegroundColor Cyan "$(simplify_home_path)"
-    Write-Host -NoNewline -ForegroundColor DarkGray ">"
-    return " "
-}
-
-### Commands ###################################################################
-function rice_include {
-    <# Return a string of a .ps1 script under librice directory.
-The name should be the file basename without extension .ps1.
-Due to the limitation, sourcing has to be done in the global scope. e.g.
-  . (rice_include file) [args] #>
-    param ($name)
-    return (Join-Path $RICE.root_dir.FullName "librice" "${name}.ps1")
-}
-
-function simplify_home_path {
-    <# Replace home prefix with "~" #>
-    $pwd.Path -replace ([regex]::Escape($HOME)+'(.*)'),'~$1'
-}
-
-## Windows specific
+Import-Module my-prompt
 Import-Module win-to-unix
 
-### Random stuff ###############################################################
+## Random stuff
 if (Test-Path -Type Leaf $RICE.overlay) { . $RICE.overlay }
