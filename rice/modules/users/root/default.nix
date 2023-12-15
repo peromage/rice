279c20ab332abc057027a60559e4a93ed1c263a8
Fang Deng <fang@elfang.com>
2023-12-15 11:01:09 -0500

    rice: Update root user option descriptions

--- rice/modules/users/root/default.nix
+++ rice/modules/users/root/default.nix
@@ -26,19 +26,36 @@ in {
     disable = mkOption {
       type = types.bool;
       default = false;
-      description = "Disable root user access.";
+      description = ''
+        Disable root user access.
+        Note that if this is enabled both `initialPassword' and `hashedPassword'
+        will be ignored.
+        If root access needs to be controlled by a custom password, do not enable
+        this option.
+      '';
     };
 
     initialPassword = mkOption {
       type = types.str;
       default = "P@55w0rd";
-      description = "Root user default password.";
+      description = ''
+        Initial password for root if `immutable' user option is disabled.  In
+        that case this option is mandatory.
+        If `immutable' user option is enabled, this will be ignored and
+        `hashedPassword' must be supplied.
+      '';
     };
 
     hashedPassword = mkOption {
       type = with types; nullOr (either str path);
       default = null;
-      description = "Hashed password or hashed password file (Used when immutable is enabled).";
+      description = ''
+        Hashed password or hashed password file.
+        If `immutable' user option is enabled, this is mandatory.  Otherwise
+        it is ignored and use `initialPassword' instead.
+        Note that if `disable' option is enabled for root, both `initialPassword'
+        and `hashedPassword' will be ignored.
+      '';
     };
   };
 
