4929f7445be69cdde968d08a92090bef6f5c3121
Fang Deng <fang@elfang.com>
2023-12-16 00:07:17 -0500

    rice: Add audio service

---
+++ rice/modules/services/audio.nix
@@ -0,0 +1,36 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.rice.services.sound;
+
+  options = with lib; {
+    enable = mkEnableOption "audio services";
+  };
+
+in {
+  options.rice.services.sound = options;
+
+  config = with lib; mkIf cfg.enable {
+    sound = {
+      enable = true;
+      mediaKeys.enable = false; # Gnome and KDE has their own handling
+    };
+
+    hardware.pulseaudio.enable = false; # Use pipewire
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      systemWide = false; # Not recommended by official news
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+  };
+}
