8ada3e213efa50b500cc9ba0b44347df3e710482
Fang Deng <fang@elfang.com>
2023-12-15 19:39:39 -0500

    rice: Add an option to disable Nix optimization

--- rice/modules/services/nix.nix
+++ rice/modules/services/nix.nix
@@ -1,53 +1,68 @@
 { config, lib, pkgs, rice, ... }:
 
 let
+  librice = rice.lib;
   nixpkgs = rice.nixpkgs;
   cfg = config.rice.services.nix;
 
 in with lib; {
   options.rice.services.nix = {
     enable = mkEnableOption "Nix settings";
+    enableOptimization = mkEnableOption "Nix optimization" // { default = true; };
   };
 
-  config = mkIf cfg.enable {
-    nixpkgs = {
-      hostPlatform = lib.mkDefault "x86_64-linux";
-      config = {
-        allowUnfree = true;
-        allowBroken = true;
+  config = librice.mkMergeIf [
+    {
+      cond = cfg.enable;
+      as = {
+        nixpkgs = {
+          hostPlatform = lib.mkDefault "x86_64-linux";
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
+          };
+        };
+
+        nix = {
+          settings.experimental-features = [ "nix-command" "flakes" ];
+
+          /* Synonyms
+             pkgs.nixVersions.stable -> pkgs.nix, pkgs.nixFlakes, pkgs.nixStable
+             pkgs.nixVersions.unstable -> pkgs.nixUnstable
+             See: https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/aliases.nix
+          */
+          package = pkgs.nixFlakes;
+
+          ## Use the nixpkgs from the toplevel flake
+          registry.nixpkgs.flake = nixpkgs;
+          nixPath = [
+            "nixpkgs=${nixpkgs}"
+            "/nix/var/nix/profiles/per-user/root/channels"
+          ];
+        };
       };
-    };
+    }
 
-    nix = {
-      settings = {
-        experimental-features = [ "nix-command" "flakes" ];
-        auto-optimise-store = true;
+    {
+      cond = cfg.enable && cfg.enableOptimization;
+      as = {
+        nix = {
+          settings.auto-optimise-store = true;
+
+          optimise = {
+            automatic = true;
+            dates = [ "weekly" ];
+          };
+
+          gc = {
+            automatic = true;
+            persistent = true;
+            dates = "weekly";
+            options = "--delete-older-than 30d";
+            randomizedDelaySec = "0";
+          };
+        };
       };
-
-      optimise = {
-        automatic = true;
-        dates = [ "weekly" ];
-      };
-
-      gc = {
-        automatic = false; # Manual GC
-        dates = "weekly";
-        options = "--delete-older-than 30d";
-      };
-
-      /* Synonyms
-         pkgs.nixVersions.stable -> pkgs.nix, pkgs.nixFlakes, pkgs.nixStable
-         pkgs.nixVersions.unstable -> pkgs.nixUnstable
-         See: https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/aliases.nix
-      */
-      package = pkgs.nixFlakes;
-
-      ## Use the nixpkgs from the toplevel flake
-      registry.nixpkgs.flake = nixpkgs;
-      nixPath = [
-        "nixpkgs=${nixpkgs}"
-        "/nix/var/nix/profiles/per-user/root/channels"
-      ];
-    };
-  };
+    }
+  ];
 }
