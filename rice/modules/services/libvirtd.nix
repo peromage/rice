87d2141cb8a962bbfcd12649084bb52fd6212f0c
Fang Deng <fang@elfang.com>
2024-02-13 10:44:19 +0800

    rice: Add libvirtd service

---
+++ rice/modules/services/libvirtd.nix
@@ -0,0 +1,50 @@
+{ config, lib, rice, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  inherit (rice.lib) mkMergeIf;
+
+  cfg = config.rice.services.libvirtd;
+
+  options = {
+    enable = mkEnableOption "virtual manager";
+    enableIntelSRIOV = mkEnableOption "Intel SR-IOV";
+  };
+
+in {
+  options.rice.services.libvirtd = options;
+
+  config = mkMergeIf [
+    {
+      cond = cfg.enable;
+      as = {
+        virtualisation.libvirtd = {
+          enable = true;
+          onBoot = "ignore";
+          onShutdown = "suspend";
+        };
+        programs.virt-manager.enable = true;
+      };
+    }
+
+    /* FIXME: Missing a DKMS module:
+       https://github.com/strongtz/i915-sriov-dkms
+    */
+    {
+      cond = cfg.enableIntelSRIOV;
+      as = {
+        boot = {
+          kernelParams = [
+            "intel_iommu=on"
+            "i915.enable_guc=3"
+            "i915.max_vfs=7"
+          ];
+
+          kernelModules = [
+            "i915"
+          ];
+        };
+      };
+    }
+  ];
+}
