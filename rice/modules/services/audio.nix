6b07f268d8dd8c9532a2b82506622f9e28d91318
Fang Deng <fang@elfang.com>
2024-05-01 23:02:30 -0400

    rice: Update sound config

--- rice/modules/services/audio.nix
+++ rice/modules/services/audio.nix
@@ -14,7 +14,7 @@ in {
 
   config = mkIf cfg.enable {
     sound = {
-      enable = true;
+      enable = false; # No ALSA sound
       mediaKeys.enable = false; # Gnome and KDE has their own handling
     };
 
@@ -24,11 +24,12 @@ in {
     services.pipewire = {
       enable = true;
       systemWide = false; # Not recommended by official news
+      audio.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
       # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
+      jack.enable = true;
 
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)
