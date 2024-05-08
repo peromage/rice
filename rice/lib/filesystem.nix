d2a9a20b188c0f93f8cfa5c67d7a7889ec90885a
Fang Deng <fang@elfang.com>
2024-05-07 23:50:33 -0400

    rice: merge imports and filesystem submodule

--- rice/lib/filesystem.nix
+++ rice/lib/filesystem.nix
@@ -1,12 +1,39 @@
 { self, nixpkgs, ... }:
 
 let
-  inherit (nixpkgs.lib) mapAttrsToList filterAttrs hasAttr elemAt;
+  inherit (nixpkgs.lib) isFunction genAttrs mapAttrsToList filterAttrs elemAt
+    pathExists optionalAttrs optional;
   inherit (builtins) readDir baseNameOf match;
 
 in with self; {
-  /* Supported system attribute constant. */
-  supportedSystems = forSupportedSystems id;
+  /* Import the given path with predefined arguments.
+
+     Type:
+       callWithArgs :: AttrSet -> ((AttrSet -> Any) | Path) -> Any
+  */
+  callWithArgs = args: fn: (if isFunction fn then fn else import fn) args;
+
+  /* Import each module from the list with given argument.
+
+     Type:
+       callListWithArgs :: AttrSet -> [(AttrSet -> a) | Path] -> [Any]
+  */
+  callListWithArgs = args: map (callWithArgs args);
+
+  /* Import paths from the given list.
+
+     Type:
+       importList :: [Path] -> [Any]
+  */
+  importList = map import;
+
+  /* Like `importList' but instead of returning a list this returns an attrset
+     with file names as the attributes.
+
+     Type:
+       importListAsAttrs :: [Path] -> AttrSet
+  */
+  importListAsAttrs = list: genAttrs list import;
 
   /* A generic function that filters all the files/directories under the given
      directory.  Return a list of names prepended with the given directory.
