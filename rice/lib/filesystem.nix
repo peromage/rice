ef90ebdeab8bcfd3cc15d103052c3262b7832a6e
Fang Deng <fang@elfang.com>
2024-05-08 00:17:11 -0400

    rice: Update filesystem module

--- rice/lib/filesystem.nix
+++ rice/lib/filesystem.nix
@@ -6,6 +6,30 @@ let
   inherit (builtins) readDir baseNameOf match;
 
 in with self; {
+  /* Generate an attribute set for supported platforms.
+       More values can be checked from `nixpkgs.lib.systems.flakeExposed'.
+
+     Type:
+       forSupportedSystems :: (String -> Any) -> AttrSet
+  */
+  forSupportedSystems = genAttrs [
+    "x86_64-linux"
+    "x86_64-darwin"
+    "aarch64-linux"
+    "aarch64-darwin"
+  ];
+
+  /* Supported system attribute constant. */
+  supportedSystems = forSupportedSystems id;
+
+  /* Check if the given system is in the supported list.
+
+     Type:
+       isSupportedSystem :: String -> Bool
+  */
+  isSupportedSystem = system: hasAttr system supportedSystems;
+
+
   /* Import the given path with predefined arguments.
 
      Type:
@@ -48,26 +72,19 @@ in with self; {
   /* Predications used for `filterDir'. */
   isDirType = name: type: type == "directory";
   isFileType = name: type: type == "regular";
+  isSymbolicType = name: type: type == "symlink";
   isDefaultNix = name: type: name == "default.nix";
-  isNixFile = name: type: (isFileType name type) && ((match ".+\\.nix$" name) != null);
-  isBaseNameSupportedSystem = name: type: hasAttr (baseNameNoExt name) supportedSystems;
-  isImportable = name: type: (isNixFile name type) || (isDirType name type);
+  isNixFile = name: type: isNotDirType name type && match ".+\\.nix$" name != null;
+  isBaseNameSupportedSystem = name: type: isSupportedSystem (baseNameNoExt name);
+  isImportable = name: type: isDirType name type || isNixFile name type;
 
-  negateFilterDirPred = wrapReturn 2 not;
-
-  isNotDirType = negateFilterDirPred isDirType;
-  isNotFileType = negateFilterDirPred isFileType
-  isNotDefaultNix = negateFilterDirPred isDefaultNix;
-  isNotNixFile = negateFilterDirPred isNixFile;
-  isNotBaseNameSupportSystem = negateFilterDirPred isBaseNameSupportedSystem;
-  isNotImportable = negateFilterDirPred isImportable;
-
-  /* Join a list of paths.
-
-     Type:
-       joinPaths :: [Path] -> Path
-  */
-  joinPaths = join "/";
+  isNotDirType = name: type: ! isDirType name type;
+  isNotFileType = name: type: ! isFileType name type;
+  isNotSymbolicType = name: type: ! isSymbolicType name type;
+  isNotDefaultNix = name: type: ! isDefaultNix name type;
+  isNotNixFile = name: type: ! isNixFile name type;
+  isNotBaseNameSupportSystem = name: type: ! isBaseNameSupportedSystem name type;
+  isNotImportable = name: type: ! isImportable name type;
 
   /* Return the basename without extension.
 
@@ -77,6 +94,6 @@ in with self; {
   baseNameNoExt = name:
     let
       b = baseNameOf name;
-      m = match "(.+)\\.[^.]+$" b;
+      m = match "(.+)(\\.[^.]+)$" b;
     in if null == m then b else elemAt m 0;
 }
