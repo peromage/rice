8663a7c8e1ae576bbb2a374043d823ff69803d14
Fang Deng <fang@elfang.com>
2024-06-06 21:35:23 -0400

    rice: Simplify code

--- rice/lib/default.nix
+++ rice/lib/default.nix
@@ -7,22 +7,16 @@
 let
   lib = nixpkgs.lib;
 
-  prelude = let
-    f = import ./prelude.nix;
-    x = f { inherit nixpkgs; self = x; };
-  in x;
-
-  call = args: node: with prelude; lib.foldl'
-    (a: i: a // i)
-    {}
-    (callAllWithArgs args
-      (listDir (n: t: isNotDefaultNix n t && isImportable n t) node));
+  prelude = let x = (import ./prelude.nix) (args // { self = x; }); in x;
 
   ## Import all nix files within this folder
-  librice = let
-    arguments = args // specialArgs // {
-      self = librice;
-    };
-  in call arguments ./.;
+  librice = let arguments = args // specialArgs // {
+    self = librice;
+  };
+  in with prelude; lib.foldl'
+    (a: i: a // i)
+    {}
+    (callAllWithArgs arguments
+      (listDir (n: t: isNotDefaultNix n t && isImportable n t) ./.));
 
 in librice
