beb48637c0140505039bb7479d10f9fec12c0862
Fang Deng <fang@elfang.com>
2024-06-06 21:54:05 -0400

    rice: Explicitly declare dependencies of librice

--- rice/lib/default.nix
+++ rice/lib/default.nix
@@ -1,8 +1,10 @@
 ### Top level of the common code
 
-## This lib only requires nixpkgs with an exception of other flakes from the top
-## level flake inputs, e.g. home-manager, which should be included in specialArgs.
-{ nixpkgs, specialArgs, ... }@args:
+{ nixpkgs
+, specialArgs ? {} # Passed around when invoking module importing.
+, home-manager ? {} # Used for home config module importing.
+, nix-darwin ? {} # Used for darwin module importing.
+, ... }@args:
 
 let
   lib = nixpkgs.lib;
@@ -10,12 +12,20 @@ let
   prelude = let x = (import ./prelude.nix) (args // { self = x; }); in x;
 
   ## Import all nix files within this folder
-  librice = let arguments = args // specialArgs // {
-    self = librice;
-  };
+  librice = let
+    arguments = args // {
+      ## TODO: Optional arguments are captured in args when not specified.
+      ## See: https://nixos.wiki/wiki/Nix_Language_Quirks
+      inherit specialArgs home-manager nix-darwin;
+      self = librice;
+    };
+    ## Initial content as a part of librice
+    init = {
+      inherit arguments;
+    };
   in with prelude; lib.foldl'
     (a: i: a // i)
-    {}
+    init
     (callAllWithArgs arguments
       (listDir (n: t: isNotDefaultNix n t && isImportable n t) ./.));
 
