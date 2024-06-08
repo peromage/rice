13f93c7ff9dad173addadaadd9e7278737bccc0f
Fang Deng <fang@elfang.com>
2024-06-08 10:39:24 -0400

    rice: Simplify rice definition

--- rice/rice.nix
+++ rice/rice.nix
@@ -1,36 +1,15 @@
-{ nixpkgs, flake }:
+{ ... }:
 
 let
-  rice = self: {
-    args = {
-      inherit nixpkgs flake;
-      rice = self; # Self reference
-      librice = self.lib;
-      dirrice = self.dirs;
-    };
-
-    lib = import ./lib (self.args // {
-      ## Blend with flake inputs
-      specialArgs = self.args.flake.inputs // self.args;
-    });
-
-    dirs = with self.dirs;
-      let withTopLevel = p: "${topLevel}/${p}";
-      in {
-        topLevel = builtins.path { path = ./.; }; # Explicit copy
-        devshells = withTopLevel "devshells";
-        dotfiles = withTopLevel "dotfiles";
-        instances = withTopLevel "instances";
-        modules = withTopLevel "modules";
-        overlays = withTopLevel "overlays";
-        packages = withTopLevel "packages";
-        templates = withTopLevel "templates";
-      };
-
-    override = args: let newRice = rice (newRice // args) // args; in newRice;
+  fixOverride = f: overrides: let x = f (x // overrides); in x // overrides // {
+    override = fixOverride f;
   };
 
-  ## Make it easier to test so that we don't have to rely on the fix function from nixpkgs
-  fix = f: let x = f x; in x;
+  rice = self: {
+    ## Overridden by the caller
+    lib = {};
+    dirs = {};
+    override = fixOverride rice;
+  };
 
-in fix rice
+in fixOverride rice {}
