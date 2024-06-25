cc0981408f2d972d734ce93db1055a7d802cb9ac
Fang Deng <fang@elfang.com>
2024-06-24 22:43:13 -0400

    rice: Refact shell import

--- rice/devshells/default.nix
+++ rice/devshells/default.nix
@@ -1,63 +1,32 @@
-{ nixpkgs, rice, withPkgsAllOverlays, ... }:
+{ system, nixpkgs, rice, pkgsWithMyOverlays, ... }@args:
 
 let
-  inherit (rice.lib) importAllNameMapped baseNameNoExt listDir forSupportedSystems
-    isSupportedSystemDir isNotSupportedSystemDir isNotDefaultNix isImportable
-    mergeSetsFirstLevel;
-  inherit (nixpkgs.lib) mapAttrs foldl';
+  lib = nixpkgs.lib;
+  librice = rice.lib;
 
   /* Import shell derivations from files/directories.
+
      Each shell must be a function that returns a derivation by `pkgs.mkShell',
-     `pkgs.buildFHSEnv' or any other equivalent.
+     `pkgs.buildFHSEnv' or any other equivalent and can be called with
+     `pkgs.callPackage'.
 
-     Any file/directory that is not named as the supported platform, such as
-     x86_64-linux, is considered to be applicable accross all supported platforms
-     defined by `supportedSystems'.
+     During the import, two directories will be included:
+     - generic
+     - [system]
 
-     If a directory has the name that can be found in `supportedSystems', it is
-     applied for that platform only.  The evaluation of the directory should be
-     an attribute set containing shell definitions just like any others,
+     The system directory is optional, which is something like x86_64-linux.
 
-     Having said that, the importing is divided into two parts: generic and
-     platform specific.  Equivalent as the follow:
-
-     {
-       x86_64-linux = { foo = <lambda>; bar = <lambda>; };
-       aarch64-linux = { foo = <lambda>; bar = <lambda>; };
-     }
-
-     and
-
-     {
-       x86_64-linux = { baz = <lambda>; };
-       aarch64-linux = { qux = <lambda>; };
-     }
-
-     These two sets will be merged at the first level before invoked by
-     `callPackage'.
-
-     {
-       x86_64-linux = { foo = <lambda>; bar = <lambda>; baz = <lambda>; };
-       aarch64-linux = { foo = <lambda>; bar = <lambda>; qux = <lambda>; };
-     }
+     `default.nix' will be ignored.
   */
-  platformShells =
-    let
-      importPackage = importAllNameMapped baseNameNoExt;
-      generic = importPackage (listDir
-        (n: v:
-          isNotSupportedSystemDir n v
-          && isImportable n v
-          && isNotDefaultNix n v)
-        ./.);
-      platforms = importPackage (listDir isSupportedSystemDir ./.);
-    in mergeSetsFirstLevel [(forSupportedSystems (system: generic))  platforms];
 
+  pkgs = pkgsWithMyOverlays system;
+  callAllIn = path: lib.mapAttrs
+    (n: v: pkgs.callPackage v args)
+    (librice.importAllNameMapped
+      librice.baseNameNoExt
+      (librice.listDir (n: t: librice.isNotDefaultNix n t && librice.isImportable n t) path));
 
-  mkDevShells = system: set:
-    let
-      pkgs = withPkgsAllOverlays system;
-      callPackage = p: pkgs.callPackage p rice.args;
-    in mapAttrs (n: v: callPackage v) set;
+  pathGeneric = ./generic;
+  pathSystem = ./. + "/${system}";
 
-in mapAttrs mkDevShells platformShells
+in callAllIn pathGeneric // lib.optionalAttrs (builtins.pathExists pathSystem) (callAllIn (pathSystem))
--- rice/devshells/x86_64-linux/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  /* Example */
-  hello_x86_64_linux = { pkgs, ... } : pkgs.mkShell {
-    packages = with pkgs.unrestrictedPkgs; [
-      hello
-    ];
-  };
-}
