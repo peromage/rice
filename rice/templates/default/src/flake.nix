0dffc09e6ddfc410ab3524943c0f754c6ed9d6eb
Fang Deng <fang@elfang.com>
2024-06-26 18:09:46 -0400

    rice: Update template

--- rice/templates/default/src/flake.nix
+++ rice/templates/default/src/flake.nix
@@ -1,20 +1,36 @@
-/* Do not use this directly.
+/* Sample flake.
+
+   This is to demostrate how to use rice configuration in a nondeterministic way.
+   Do not build this directly on your setup as it will fail.
 */
 
 {
   inputs = {
     conf.url = "github:peromage/pew/master?dir=rice";
 
-    ## Dynamic binding
+    /* Override the release version */
     #nixpkgs-2305.url = "github:nixos/nixpkgs/nixos-23.05";
     #conf.inputs.nixpkgs.follows = "nixpkgs-2305";
   };
-  outputs = { self, conf }: {
+
+  outputs = { self, conf }:
+    let
+      lib = conf.inputs.nixpkgs.lib;
+
+    in {
     nixosConfigurations = {
-      Foobar = conf.nixosConfigurations.Framepie.extraModule {
-        rice.hosts.hostName = conf.rice.nixpkgs.lib.mkForce "Foobar";
+      FramepieAlter = conf.nixosConfigurations.Framepie.extraModule {
+        rice.hosts.hostName = conf.rice.nixpkgs.lib.mkForce "FramepieAlter";
         rice.users.immutable = true;
         rice.users.profiles.fang.hashedPassword = "secret";
+      };
+
+      Foobar = lib.nixosSystem {
+        specialArgs = conf.specialArgs;
+        modules = [
+          conf.nixosModules.default
+          /* Your modules */
+        ];
       };
     };
   };
