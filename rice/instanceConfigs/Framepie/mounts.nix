f4d8519279bbd1e9bca5d6a347d12cb089349788
Fang Deng <fang@elfang.com>
2024-06-26 23:36:18 -0400

    rice: Update fs mounting

--- rice/instanceConfigs/Framepie/mounts.nix
+++ rice/instanceConfigs/Framepie/mounts.nix
@@ -1,39 +1,45 @@
-{ ... }:
+{ lib, ... }:
 
-{
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/35154f6e-27aa-49f8-b1b6-6472127cb524";
-    fsType = "btrfs";
-    options = [ "subvol=@nixos" "ssd" "noatime" "compress=zstd:3" ];
+let
+  withDefaultSubvolOptions =
+    { vol
+    , compress ? true
+    , cow ? true
+    , extraOptions ? []
+    }: {
+      device = "/dev/disk/by-uuid/35154f6e-27aa-49f8-b1b6-6472127cb524";
+      fsType = "btrfs";
+      options = [
+        "subvol=${vol}"
+        "ssd"
+        "noatime"
+        "autodefrag"
+      ]
+      ++ (lib.optional compress "compress=zstd:3")
+      ++ (lib.optional (!cow) "nodatacow")
+      ++ extraOptions;
+    };
+
+in {
+  fileSystems."/" = withDefaultSubvolOptions { vol = "@nixos"; };
+  fileSystems."/nix" = withDefaultSubvolOptions { vol = "@nixstore"; };
+  fileSystems."/home" = withDefaultSubvolOptions { vol = "@home"; };
+
+  fileSystems."/ff/swap" = withDefaultSubvolOptions {
+    vol = "@swap";
+    compress = false;
+    cow = false;
   };
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/35154f6e-27aa-49f8-b1b6-6472127cb524";
-    fsType = "btrfs";
-    options = [ "subvol=@nixstore" "ssd" "noatime" "compress=zstd:3" ];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/35154f6e-27aa-49f8-b1b6-6472127cb524";
-    fsType = "btrfs";
-    options = [ "subvol=@home" "ssd" "noatime" "compress=zstd:3" ];
+  fileSystems."/ff/vm" = withDefaultSubvolOptions {
+    vol = "@vm";
+    compress = false;
+    cow = false;
   };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/8F86-D998";
     fsType = "vfat";
-  };
-
-  fileSystems."/ffstore/swap" = {
-    device = "/dev/disk/by-uuid/35154f6e-27aa-49f8-b1b6-6472127cb524";
-    fsType = "btrfs";
-    options = [ "subvol=@swap" "ssd" "noatime" "compress=zstd:3" ];
-  };
-
-  fileSystems."/ffstore/vm" = {
-    device = "/dev/disk/by-uuid/35154f6e-27aa-49f8-b1b6-6472127cb524";
-    fsType = "btrfs";
-    options = [ "subvol=@vm" "ssd" "noatime" "compress=zstd:3" ];
   };
 
   swapDevices = [
