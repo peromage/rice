620f25c780b4cac883e40f723e1c36d281cc0b9b
Fang Deng <fang@elfang.com>
2024-06-26 20:53:32 -0400

    rice: Remove redundant home config

--- rice/homeConfigs/fang/default.nix
+++ rice/homeConfigs/fang/default.nix
@@ -1,7 +1,57 @@
-## This file is only used for default importing
+{ pkgs, ... }:
 
-{ ... }:
+let
+  src = rice.paths.dotfiles;
 
-{
-  imports = [ ./home.nix ];
+in {
+  imports = [ ./apps ];
+
+  /* Managed by Home Manager */
+  programs.home-manager.enable = true;
+
+
+  /* Mapped paths from Home Manager's variables:
+
+     `~/.config': config.xdg.configHome
+     `~/.local/share': config.xdg.dataHome
+
+     Shorthands for creating files under directories:
+
+     `~': home.file.<name>
+     Ref: https://nix-community.github.io/home-manager/options.html#opt-home.file
+
+     `~/.config': xdg.configFile.<name>
+     Ref: https://nix-community.github.io/home-manager/options.html#opt-xdg.configFile
+
+     `~/.local/share': xdg.dataFile.<name>
+     Ref: https://nix-community.github.io/home-manager/options.html#opt-xdg.dataFile
+  */
+  xdg.enable = true;
+
+  home = {
+    username = "fang";
+    homeDirectory = "/home/fang";
+    stateVersion = "24.05";
+  };
+
+  /* Alternatively source in a manual way:
+     ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+     or
+     /etc/profiles/per-user/fang/etc/profile.d/hm-session-vars.sh
+  */
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  home.sessionPath = [
+    "\${HOME}/bin"
+    "\${HOME}/.local/bin"
+  ];
+
+  home.file = {
+    "bin" = {
+      source = "${src}/bin/bin";
+      recursive = true;
+    };
+  };
 }
