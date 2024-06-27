a22e4ef4196f4901d7b7f2eb55ed7e4429dbf532
Fang Deng <fang@elfang.com>
2024-06-26 20:39:44 -0400

    rice@fcitx5: Merge config files

---
+++ rice/homeConfigs/fang/apps/fcitx5.nix
@@ -0,0 +1,35 @@
+{ pkgs, rice, ... }:
+
+let
+  src = "${rice.paths.dotfiles}/fcitx5";
+
+in {
+  /* Temporarily disabled since this is problematic in user space.  Instead, the
+     IME module should be enabled in NixOS config.
+  */
+  # i18n.inputMethod = {
+  #   enabled = "fcitx5";
+  #   fcitx5.addons = with pkgs; [
+  #     fcitx5-rime
+  #     fcitx5-configtool
+  #     fcitx5-chinese-addons
+  #     fcitx5-gtk
+  #   ];
+  # };
+
+  # home.packages = with pkgs; [
+  #   librime
+  #   rime-cli
+  #   rime-data
+  # ];
+
+  xdg.configFile."fcitx5" = {
+    source = "${src}/.config/fcitx5";
+    recursive = true;
+  };
+
+  xdg.dataFile."fcitx5" = {
+    source = "${src}/.local/share/fcitx5";
+    recursive = true;
+  };
+}
