a0b6fc37dc9f0d9ff1c38b8d5e30a60e01135c5e
Fang Deng <fang@elfang.com>
2024-07-14 15:09:18 -0400

    Create pewlib

---
+++ __pew__/site-lisp/pewlib/pewlib.el
@@ -0,0 +1,12 @@
+;;; pewlib.el --- pewlib main entry -*- lexical-binding: t; -*-
+
+;; Author: Fang Deng <fang@elfang.com>
+
+;;; Commentary:
+;;; Code:
+
+(require 'pewlib-trivial)
+(require 'pewlib-editor)
+
+(provide 'pewlib)
+;;; pewlib.el ends here
