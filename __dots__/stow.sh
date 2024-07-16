19524fd7477aa2b44269f64ff26c9487c037201a
Fang Deng <fang@elfang.com>
2024-07-15 23:36:24 -0400

    Refactor stow.sh

--- __dots__/stow.sh
+++ __dots__/stow.sh
@@ -1,33 +1,72 @@
 #!/usr/bin/env bash
 
-ACTION="$1" && shift
-SOURCE="$1" && shift
+## The rest are package names
+# ACTION="$1" && shift
+# SOURCE="$1" && shift
 
-case "$ACTION" in
-    -S|--stow|-D|--delete|-R|--restow) ;;
-    *)
-        >&2 cat <<EOF
-Invalid action: $ACTION
+set -e
 
+function usage {
+    >&2 cat <<EOF
 Usage:
-  $0 action path-to-package-top [arguments]
+  $0 ACTION [pkg1 [pkg2 [...]]] ACTION [pkg3 [pkg4 [...]]] ...
 
 Actions:
-  -S, --stow
-  -D, --delete
-  -R, --restow
+  -s    Stow packages
+  -d    Delete packages
+  -r    Restow packages
+
+Multiple actions can be specified at the same time.  For example:
+  $0 -s bash fish -d pwsh -r tmux
+
 EOF
-        exit 1
-        ;;
-esac
+}
 
-cd "${BASH_SOURCE%/*}"
+stow_list=()
+delete_list=()
+restow_list=()
+current_list=
+verbose_mode=
 
-if ! test -d "$SOURCE"; then
-    echo "Not a directory: $SOURCE"
+## Need to append action and package name
+base_cmd="stow --no-folding --dir=${BASH_SOURCE[0]%/*} --target=$HOME"
+
+function run_stow {
+    local act="$1" && shift
+    for p in "$@"; do
+        local cmd="${base_cmd} ${act} ${p}"
+        [[ -n "$verbose_mode" ]] && echo "> CMD: $cmd"
+        eval "$cmd"
+    done
+}
+
+while [[ "$OPTIND" -le $# ]]; do
+    if getopts "sdrv" o; then
+        case "$o" in
+            s) current_list=stow_list ;;
+            d) current_list=delete_list ;;
+            r) current_list=restow_list ;;
+            v) verbose_mode=1 ;;
+            *) usage; exit 1 ;;
+        esac
+    else
+        if [[ -n "$current_list" ]]; then
+            eval "${current_list}+=("${!OPTIND}")" # Indirect reference
+            let OPTIND++
+        else
+            usage
+            exit 1
+        fi
+    fi
+done
+
+action_performed=0
+
+if [[ ${#stow_list} -eq 0 ]] && [[ ${#delete_list} -eq 0 ]] && [[ ${#restow_list} -eq 0 ]]; then
+    usage
     exit 1
 fi
 
-CMD="stow $@ --no-folding --dir=$SOURCE --target=$HOME $ACTION ."
-echo "[DEBUG]: $CMD"
-eval "$CMD"
+run_stow -S "${stow_list[@]}"
+run_stow -D "${delete_list[@]}"
+run_stow -R "${restow_list[@]}"
