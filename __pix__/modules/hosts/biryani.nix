1a7eefc6f3b195947a2d57a1461a8b4922f92965
Fang Deng <fang@elfang.com>
2024-08-16 16:27:39 -0400

    pix: Less restrictions for host profiels

--- __pix__/modules/hosts/biryani.nix
+++ __pix__/modules/hosts/biryani.nix
@@ -1,26 +1,31 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  config.pix.hosts.profiles.biryani = {
-    config = {
-      environment.systemPackages = with pkgs; [
-        firefox
-        vim
-        tmux
-        git
-        curl
-        wget
-        rsync
-        tree
+let
+  cfg = config.pix.hosts.profiles.biryani;
 
-        ## Filesystems
-        ntfs3g
-        exfat
-        exfatprogs
-        e2fsprogs
-        fuse
-        fuse3
-      ];
-    };
+in {
+  options.pix.hosts.profiles.biryani = with lib; {
+    enable = mkEnableOption "host biryani";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      firefox
+      vim
+      tmux
+      git
+      curl
+      wget
+      rsync
+      tree
+
+      ## Filesystems
+      ntfs3g
+      exfat
+      exfatprogs
+      e2fsprogs
+      fuse
+      fuse3
+    ];
   };
 }
--- __pix__/modules/hosts/default.nix
+++ __pix__/modules/hosts/default.nix
@@ -4,26 +4,6 @@ let
   libpix = pix.lib;
   cfg = config.pix.hosts;
 
-  ## Explicitly specify which name can be defined in `<host>.config' below.
-  ## This is a workaround to aovid infinit recursion.
-  ## Details see `libpix.mkMergeTopLevel'.
-  allowedNames = ["fonts" "programs" "environment"];
-
-  hostProfileOptions = {name, config, ...}: {
-    options = with lib; {
-      enable = mkEnableOption "host ${name}";
-
-      config = mkOption {
-        type = types.attrs;
-        default = {};
-        description = ''
-          Host detailed configurations.
-          Attributes are equivalent to NixOS toplevel config.
-        '';
-      };
-    };
-  };
-
 in {
   imports = with libpix; listDir isNotDefaultNix ./.;
 
@@ -49,56 +29,30 @@ in {
       '';
     };
 
-    profiles = mkOption {
-      type = with types; attrsOf (submodule hostProfileOptions);
-      default = {};
-      description = ''
-        Host profile definitions.
-      '';
-    };
+    ## Each profile can have their own options, while `enable' is mandatory.
+    profiles = {};
   };
 
   /* Implementation */
-  config = let
-    enabledHosts = lib.filterAttrs (name: config: config.enable) cfg.profiles;
-    anyHostEnabled = lib.length enabledHosts != 0;
+  config = {
+    ## Common
+    nixpkgs.hostPlatform = cfg.platform;
+    networking.hostName = cfg.hostName;
 
-    ## Use the value from `pix.hosts.hostName' if defined.  Otherwise, use the
-    ## name of last host profile.
-    hostName = with lib;
-      if cfg.hostName != null then cfg.hostName
-      else foldl (_: config: config.name) null enableHosts;
-
-  in
-    with lib; mkMerge [
-      (libpix.mkMergeTopLevel allowedNames (mapAttrsToList (_: config: config.config) enabledHosts))
+    ## Assertions
+    assertions = [
+      {
+        assertion = cfg.platform != null;
+        message = "Platform has be explicitly specified.";
+      }
 
       {
-        ## Common
-        nixpkgs.hostPlatform = cfg.platform;
-        networking.hostName = hostName;
-
-        ## Assertions
-        assertions = [
-          {
-            assertion = cfg.platform != null;
-            message = "Platform has be explicitly specified.";
-          }
-
-          {
-            assertion = hostName != null;
-            message = ''
+        assertion = cfg.hostName != null;
+        message = ''
               No hostname provided.
               Either `pix.hosts.hostName' is not set or no host profile is enabled."
             '';
-          }
-
-          ## Only allowed options can be defined
-          {
-            assertion = all (name: elem name allowedNames) (flatten (map (config: attrNames config.config) (attrValues enabledHosts)));
-            message = "Only these config names can be defined in host profiles: ${toString allowedNames}.";
-          }
-        ];
       }
     ];
+  };
 }
