f629262b757d874b273501cce184b081dfb7d959
Fang Deng <fang@elfang.com>
2024-08-17 22:21:07 -0400

    pix: Add ssh ports option

--- __pix__/modules/services/sshd.nix
+++ __pix__/modules/services/sshd.nix
@@ -6,14 +6,22 @@ let
 in {
   options.pix.services.sshd = with lib; {
     enable = mkEnableOption "SSH service";
+
     enablePassword = mkEnableOption "SSH password login";
+
     enableOnDemandActivation = mkEnableOption "Only start service when there is incoming connection";
+
+    ports = mkOption {
+      type = with types; listOf port;
+      default = [ 22 ];
+      description = "SSH daemon listening ports.";
+    }
   };
 
   config = lib.mkIf cfg.enable {
     services.openssh = {
       enable = true;
-      ports = [ 22 ];
+      ports = cfg.ports;
       openFirewall = true; # Whitelist the ports
       settings = {
         X11Forwarding = false;
