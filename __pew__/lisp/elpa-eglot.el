d12b09fabb014393253cab494523ba228bd61c90
Fang Deng <fang@elfang.com>
2024-08-22 14:32:29 -0400

    pew: Add eglot config

---
+++ __pew__/lisp/elpa-eglot.el
@@ -0,0 +1,22 @@
+;;; elpa-eglot.el --- Language server by Eglot -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(pewcfg::use-package eglot
+  :ensure nil
+  :commands (eglot eglot-ensure)
+  :bind ( :map eglot-mode-map
+          ("C-c l f" . eglot-format)
+          ("C-c l F" . eglot-format-buffer)
+          ("C-c l r" . eglot-rename)
+          ("C-c l a" . eglot-code-actions)
+          ("C-c l q" . eglot-code-action-quickfix)
+          ("C-c l i" . imenu) )
+  :custom
+  (eglot-strict-mode nil)
+  (eglot-autoshutdown t) ;; Save resources
+  (eglot-extend-to-xref nil) ;; Don't include files outside of current project
+  (eglot-report-progress t))
+
+(provide 'elpa-eglot)
+;;; elpa-eglot.el ends here
