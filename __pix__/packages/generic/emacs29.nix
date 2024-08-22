2af967d0d34579cabd00a3ddfc9e8acb0e6e5daf
Fang Deng <fang@elfang.com>
2024-08-21 23:24:45 -0400

    pix: Update emacs package

--- __pix__/packages/generic/emacs29.nix
+++ __pix__/packages/generic/emacs29.nix
@@ -1,20 +1,25 @@
 { pkgs, ... }:
 
 let
-  useEmacsWith = with pkgs; (emacsPackagesFor emacs29).emacsWithPackages;
+  emacsVersion = pkgs.emacs29;
 
-  /* Refs:
+  /* Emacs dependencies.  Some packages like vterm may have issues to build in
+     Nix environment due to encapsulated environment.  Include them together
+     with this declaration so that Emacs can find them.
+
+     Refs:
      https://github.com/martinbaillie/dotfiles/blob/c31b8aa815a940c45210c7ab9029141a8e6c6e93/modules/editors/emacs.nix#L30
      https://nixos.wiki/wiki/Emacs
   */
-  emacsPackages = epkgs: with epkgs; [
+  myEmacs = (pkgs.emacsPackagesFor emacsVersion).emacsWithPackages (epkgs: with epkgs; [
     vterm # Since vterm cannot be compiled in user environment, use this instead
-  ];
+  ]);
 
+  ## May omit buildEnv?
 in pkgs.buildEnv {
   name = "my-emacs";
   paths = with pkgs; [
-    (useEmacsWith emacsPackages)
+    myEmacs
     libvterm-neovim # libvterm is not maintained, use this instead
   ];
 }
