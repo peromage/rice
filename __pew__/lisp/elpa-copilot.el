5b7dfe2d6b8330e08e7234efd3fcf9b9925ff1ce
Fang Deng <fang@elfang.com>
2024-08-27 17:17:09 -0400

    pew: Avoid key conflict in copilot

--- __pew__/lisp/elpa-copilot.el
+++ __pew__/lisp/elpa-copilot.el
@@ -6,8 +6,13 @@
   :ensure nil
   :init (pewcfg :vcpkg ("copilot-emacs/copilot.el" "main"))
   :bind ( :map copilot-completion-map
-          ("<tab>" . copilot-accept-completion)
-          ("TAB"   . copilot-accept-completion) ))
+          ;; Avoid conflict with regular completion
+          ("M-n" . copilot-next-completion)
+          ("M-p" . copilot-previous-completion)
+          ("M-k" . copilot-clear-overlay)
+          ("M-j" . copilot-accept-completion)
+          ("M-w" . copilot-accept-completion-by-word)
+          ("M-y" . copilot-accept-completion-by-line) ))
 
 (provide 'elpa-copilot)
 ;;; elpa-copilot.el ends here
