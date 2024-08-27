010d63eff227e7984b5d991df70541dfb0440550
Fang Deng <fang@elfang.com>
2024-08-26 22:27:58 -0400

    pew: Fix a CI failure

--- __pew__/tests/test-init.sh
+++ __pew__/tests/test-init.sh
@@ -15,6 +15,7 @@
        (user-init-file (expand-file-name "init.el" user-emacs-directory))
        (load-path (delq user-emacs-directory load-path)))
   (load-file user-init-file)
+  (setq dired-mode-hook nil) ;; Avoid failures due to hooks not being loaded in script mode
   (run-hooks 'after-init-hook)
   (run-hooks 'emacs-startup-hook)
   (message "%s started in %s" (emacs-version) (emacs-init-time)))
