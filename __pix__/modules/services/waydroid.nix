29399dd15b694fac8d74ce6383fa47271b083e99
Fang Deng <fang@elfang.com>
2024-09-23 19:11:37 -0400

    pix: Make waydroid user session service as a part of the config

--- __pix__/modules/services/waydroid.nix
+++ __pix__/modules/services/waydroid.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.pix.services.waydroid;
@@ -11,14 +11,19 @@ in with lib; {
   config = mkIf cfg.enable {
     virtualisation.waydroid.enable = true;
 
+    /* To start a user session in the background and avoid accidental shutdown
+       If don't care about the session life time, use `waydroid show-full-ui' to
+       directly start it.
+    */
     systemd.user.services.waydroid-session = {
-      enable = false; # Means to be started manually on demand
+      enable = true;
       description = "Waydroid user session";
       after = [ "waydroid-container.service" ];
+      # wantedBy = [ "default.target" ]; # Means to be started manually on demand
       serviceConfig = {
         Type = "simple";
         Restart = "no";
-        ExecStart = "/usr/bin/env waydroid session start";
+        ExecStart = "${pkgs.waydroid}/bin/waydroid session start"; # Assume the option `virtualisation.waydroid' uses the same package
       };
     };
   };
