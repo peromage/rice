77d0955d9f559f96b3241cc124d0f168d792f1be
Fang Deng <fang@elfang.com>
2024-09-26 16:52:31 -0400

    pix: Remodel Python package

---
+++ __pix__/packages/generic/python.nix
@@ -0,0 +1,39 @@
+/* Python runtime.
+
+   The Python package is not meant to go in to system/user environment since
+   Nix doesn't allow site package directory to be mutable.
+
+   This package here is solely to provide runtime support for arbitrary script
+   execution and simple project management.
+
+   For regular Python tools, use `pipx' to install in an ad-hoc fashion, if they
+   are not intended to be managed by Nix config.
+
+   For dev dependency management, use `poetry'.
+
+   For virtual environment, use `poetry shell'.
+
+   For more complicated use case, use `nix-shell' or `nix develop'.
+*/
+
+{ pkgs, ... }:
+
+let
+  myPython = pkgs.python3.withPackages (pyPkgs: with pyPkgs; [
+    ## Essential for building Python packages
+    setuptools
+    wheel
+    poetry-core
+
+    ## Package management
+    pip
+    pipx # Ad-hoc user space tool
+  ]);
+
+in pkgs.buildEnv {
+  name = "my-python3";
+  paths = [
+    myPython
+    pkgs.poetry
+  ];
+}
