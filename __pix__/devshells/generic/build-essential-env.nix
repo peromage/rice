f9220522fbb1c137f46cdef47d209be4ebbd89f6
Fang Deng <fang@elfang.com>
2024-09-26 12:17:45 -0400

    pix: Add a standard FHS env shell build-essential-env

---
+++ __pix__/devshells/generic/build-essential-env.nix
@@ -0,0 +1,23 @@
+/* An FHS environment with essential C/C++ utilities.
+
+   This is to address an issue where traditional search path is required when
+   building projects out of NixOS config.
+
+   The `build-essential' package is adviced to be installed in the system to avoid
+   being cleaned up from Nix store since it may contain runtime dependencies if
+   somehow they are linked by their real path in Nix store.  However, in most
+   cases, programs that requires FHS environment may still need to run within
+   this shell.
+
+   For example, the Python package `pynput' requires canonical headers from
+   `linuxHeaders' package.
+*/
+{ pkgs, ... }:
+
+let
+  fhsenv = pkgs.buildFHSEnv {
+    name = "Standard FHS environment";
+    targetPkgs = pkgs: [ pkgs.pixPkgs.build-essential ];
+  };
+
+in fhsenv.env
