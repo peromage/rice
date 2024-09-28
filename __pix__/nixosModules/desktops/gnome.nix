d00cecc96708f2c95c3a8abb7c8d715cc7eade54
Fang Deng <fang@elfang.com>
2024-09-28 17:01:50 -0400

    pix: Add touchpad gesture support for Gnome in X11

--- __pix__/nixosModules/desktops/gnome.nix
+++ __pix__/nixosModules/desktops/gnome.nix
@@ -10,19 +10,29 @@ in with lib; {
     enableGDM = mkEnableOption "GDM display manager" // { default = true; };
   };
 
-  config = mkIf cfg.enable {
-    services.xserver = {
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = cfg.enableGDM;
-      displayManager.gdm.wayland = cfgOverall.enableWayland;
-    };
+  config = mkMerge [
+    (mkIf cfg.enable {
+      services.xserver = {
+        desktopManager.gnome.enable = true;
+        displayManager.gdm.enable = cfg.enableGDM;
+        displayManager.gdm.wayland = cfgOverall.enableWayland;
+      };
 
-    environment.systemPackages = with pkgs; [
-      gnome-tweaks
-      gnome-extension-manager
-      dconf2nix
-      gnome-terminal ## Provides more functionalities than default gnome-console
-      gnomeExtensions.tray-icons-reloaded
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        gnome-tweaks
+        gnome-extension-manager
+        dconf2nix
+        gnome-terminal ## Provides more functionalities than default gnome-console
+        gnomeExtensions.tray-icons-reloaded
+      ];
+    })
+
+    # X11 accessories
+    (mkIf (cfg.enable && ! cfgOverall.enableWayland) {
+      environment.systemPackages = with pkgs; [
+        gnomeExtensions.x11-gestures
+      ];
+      services.touchegg.enable = true;
+    })
+  ];
 }
