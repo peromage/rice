f96f442b7db288eabc931cd0c2e33495dc7bd947
Fang Deng <fang@elfang.com>
2024-10-15 16:47:23 -0400

    pix: Add FRP service

---
+++ __pix__/nixosModules/services/frp.nix
@@ -0,0 +1,57 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.pix.services.frp;
+
+in with lib; {
+  options.pix.services.frp = {
+    enable = mkEnableOption "FRP server";
+
+    bindAddr = mkOption {
+      type = types.str;
+      default = "0.0.0.0";
+      description = "The address that the server listens to.";
+    };
+
+    bindPort = mkOption {
+      type = types.port;
+      default = 7000;
+      description = "The port for server and clients communication.";
+    };
+
+    openPorts = mkOption {
+      type = with types; listOf port;
+      default = [];
+      description = "Additional ports that are allowed by firewall.";
+    };
+
+    password = mkOption {
+      type = types.str;
+      default = "P@55w0rd";
+      description = "Token for client connection authentication.";
+    };
+  };
+
+  config = let
+    allowedPorts = cfg.openPorts ++ [ cfg.bindPort ];
+
+  in mkIf cfg.enable {
+    services.frp = {
+      enable = true;
+      role = "server";
+      settings = {
+        bindAddr = cfg.bindAddr;
+        bindPort = cfg.bindPort;
+        auth.method = "token";
+        auth.additionalScopes = [ "HeartBeats" "NewWorkConns" ];
+        auth.token = cfg.password;
+        maxPortsPerClient = 0;
+      };
+    };
+
+    pix.services.firewall = {
+      allowedTCPPorts = allowedPorts;
+      allowedUDPPorts = allowedPorts;
+    };
+  };
+}
