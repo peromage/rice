ac7294508d176517000d7c55312e98b6b886b582
Fang Deng <fang@elfang.com>
2024-10-25 14:03:36 -0400

    pot: Simplify usage of stow.sh

--- dotfiles/stow.sh
+++ dotfiles/stow.sh
@@ -9,65 +9,62 @@ set -e
 function usage {
     >&2 cat <<EOF
 Usage:
-  $0 ACTION [pkg1 [pkg2 [...]]] ACTION [pkg3 [pkg4 [...]]] ...
+  $0 ACTION [OPTIONS] pkg1 [pkg2 [...]]
 
 Actions:
-  -s    Stow packages
-  -d    Delete packages
-  -r    Restow packages
+  -S    Stow packages
+  -D    Delete packages
+  -R    Restow packages
 
-Multiple actions can be specified at the same time.  For example:
-  $0 -s bash fish -d pwsh -r tmux
-
+Options:
+  -f    Enable folding (Default: no folding)
+  -h    Show this message
+  -v    Verbose output
 EOF
 }
 
-stow_list=()
-delete_list=()
-restow_list=()
-current_list=
-verbose_mode=
-
 ## Need to append action and package name
-base_cmd="stow --no-folding --dir=${BASH_SOURCE[0]%/*} --target=$HOME"
+BASECMD="stow --dir=${BASH_SOURCE[0]%/*} --target=$HOME"
+
+action=
+opts="folding=0 verbose=0"
+package_list=()
 
 function run_stow {
-    local act="$1" && shift
+    local action="$1" && shift
+    local opts="$1" && shift
+    local args=
+    ! [[ "$opts" == *folding=1* ]] && args="--no-folding $args"
+    [[ "$opts" == *verbose=1* ]] && args="-v $args"
+
     for p in "$@"; do
-        local cmd="${base_cmd} ${act} ${p}"
-        [[ -n "$verbose_mode" ]] && echo "> CMD: $cmd"
+        local cmd="${BASECMD} ${args} ${action} ${p}"
+        echo "> RUN: $cmd"
         eval "$cmd"
     done
 }
 
 while [[ "$OPTIND" -le $# ]]; do
-    if getopts "sdrhv" o; then
+    if getopts "SDRfhv" o; then
         case "$o" in
-            s) current_list=stow_list ;;
-            d) current_list=delete_list ;;
-            r) current_list=restow_list ;;
-            v) verbose_mode=1 ;;
-            h) ;&
-            *) usage; exit 1 ;;
+            (S) action="-S" ;;
+            (D) action="-D" ;;
+            (R) action="-R" ;;
+            (f) opts="folding=1 $opts" ;;
+            (v) opts="verbose=1 $opts" ;;
+            (h) ;&
+            (*) usage; exit 1 ;;
         esac
     else
-        if [[ -n "$current_list" ]]; then
-            eval "${current_list}+=("${!OPTIND}")" # Indirect reference
-            let OPTIND++
-        else
-            usage
-            exit 1
-        fi
+        eval "package_list+=("${!OPTIND}")" # Indirect reference
+        let OPTIND++
     fi
 done
 
-action_performed=0
 
-if [[ ${#stow_list} -eq 0 ]] && [[ ${#delete_list} -eq 0 ]] && [[ ${#restow_list} -eq 0 ]]; then
+if [[ -z $action ]] || [[ ${#package_list} -eq 0 ]]; then
     usage
     exit 1
 fi
 
-run_stow -S "${stow_list[@]}"
-run_stow -D "${delete_list[@]}"
-run_stow -R "${restow_list[@]}"
+run_stow $action "$opts" "${package_list[@]}"
