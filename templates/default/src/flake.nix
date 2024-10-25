86ca848f70c12e43f02306b6e0bd5b255cb3737a
Fang Deng <fang@elfang.com>
2024-10-25 16:32:33 -0400

    pix: Update template

--- templates/default/src/flake.nix
+++ templates/default/src/flake.nix
@@ -1,9 +1,9 @@
 /* Sample flake to demonstrate how to use pix in a nondeterministic way. */
 {
   inputs = {
-    #nixpkgs-2305.url = "github:nixos/nixpkgs/nixos-23.05";
-    #pix.inputs.nixpkgs.follows = "nixpkgs-2305
     pix.url = "github:peromage/pix/master";
+    #pix.inputs.nixpkgs.follows = "nixpkgs-2305
+    #nixpkgs-2305.url = "github:nixos/nixpkgs/nixos-23.05";
   };
 
   outputs = { self, pix, ... }:
