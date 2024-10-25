365841dd2424133dc1695532bc8d52bd1a7f9fb4
Fang Deng <fang@elfang.com>
2024-10-25 14:45:18 -0400

    pix: Rename ricepot to pixpot

--- packages/generic/pixpot.nix
+++ packages/generic/pixpot.nix
@@ -3,7 +3,7 @@
 { pkgs, pix, ... }:
 
 pkgs.stdenvNoCC.mkDerivation {
-  pname = "ricepot";
+  pname = "pixpot";
   version = "0.0.1";
   src = pix.path.dotfiles;
 
@@ -11,7 +11,7 @@ pkgs.stdenvNoCC.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/dotfiles
-    rsync -av $src/* $out/dotfiles/
+    rsync -a $src/* $out/dotfiles/
     stow --no-folding --target=$out --dir=$out/dotfiles bin
   '';
 }
